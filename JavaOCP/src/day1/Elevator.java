b68390cf19bb140bbb3caca037db7ada946c25b0
FP <FP@FP>
2026-01-24 15:42:39 +0800

    ocp

--- JavaOCP/src/day1/Elevator.java
+++ JavaOCP/src/day1/Elevator.java
@@ -10,31 +10,36 @@ public class Elevator {
 		System.out.printf("請輸入目前樓層 => ");
 		int current = sc.nextInt();
 		
-		System.out.printf("請輸入目標樓層 => ");
-		int target = sc.nextInt();
-		
-		// 上樓
-		if(current < target) {
-			for(int floor = current ; floor <= target  ; floor++) {
-				System.out.printf("電梯上樓目前在 %d 樓%n", floor);
+		while(true) {
+			
+			System.out.printf("請輸入目標樓層 => ");
+			int target = sc.nextInt();
+			
+			// 上樓
+			if(current < target) {
+				for(int floor = current ; floor <= target  ; floor++) {
+					System.out.printf("電梯上樓目前在 %d 樓%n", floor);
+				}
 			}
-		}
-		
-		// 下樓
-		if(current > target) {
-			for(int floor = current ; floor >= target ; floor--) {
-				System.out.printf("電梯下樓目前在 %d 樓%n", floor);
+			
+			// 下樓
+			if(current > target) {
+				for(int floor = current ; floor >= target ; floor--) {
+					System.out.printf("電梯下樓目前在 %d 樓%n", floor);
+				}
 			}
+			
+			// 同一樓
+			if(current == target) {
+				System.out.printf("你已在目標樓層: %d%n", target);
+			}
+			
+			System.out.println("電梯抵達目的地");
+			// 將 current 變更為 target
+			current = target;
 		}
 		
-		// 同一樓
-		if(current == target) {
-			System.out.printf("你已在目標樓層: %d%n", target);
-		}
-		
-		System.out.println("電梯抵達目的地");
-		
-		sc.close();
+		//sc.close();
 	}
 
 }
