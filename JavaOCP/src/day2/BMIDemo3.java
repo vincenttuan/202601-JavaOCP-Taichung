372cd5d1ddbaeaa2c68a7fbbadc767ec1fde7b9a
FP <FP@FP>
2026-01-31 10:59:48 +0800

    bmi

---
+++ JavaOCP/src/day2/BMIDemo3.java
@@ -0,0 +1,23 @@
+package day2;
+
+public class BMIDemo3 {
+
+	public static void main(String[] args) {
+		// 建立物件實體
+		BMIUser user1 = new BMIUser();
+		// 資料設定(設定屬性)
+		user1.name = "小明";
+		user1.height = 170.0;
+		user1.weight = 60.0;
+		// 執行邏輯(執行方法)
+		
+		BMIUser user2 = new BMIUser();
+		user2.name = "小英";
+		user2.height = 160.0;
+		user2.weight = 45.5;
+		
+		
+
+	}
+
+}
--- JavaOCP/src/day2/BMIUser.java
+++ JavaOCP/src/day2/BMIUser.java
@@ -8,17 +8,17 @@ public class BMIUser {
 	double height;
 	
 	// 2個邏輯(物件方法)
-	private double calcBMI(double w, double h) {
-		double bmi = w / Math.pow(h/100, 2);
+	private double calcBMI() {
+		double bmi = weight / Math.pow(height/100, 2);
 		return bmi;
 	}
 	
-	public void printBMI(String name, double h, double w) {
-		double bmi = calcBMI(w, h); // 直接調用已經寫好的 bmi 計算邏輯
+	public void printBMI() {
+		double bmi = calcBMI(); // 直接調用已經寫好的 bmi 計算邏輯
 		System.out.println("BMI 執行資料結果:");
 		System.out.printf("姓名: %s %n", name);
-		System.out.printf("身高: %.1f cm %n", h);
-		System.out.printf("體重: %.1f kg %n", w);
+		System.out.printf("身高: %.1f cm %n", height);
+		System.out.printf("體重: %.1f kg %n", weight);
 		System.out.printf("bmi: %.2f %n", bmi);
 	}
 	
