66a4396b3bf51f6c2951b31e3ce80debcd759079
FP <FP@FP>
2026-01-31 14:39:16 +0800

    score

---
+++ JavaOCP/src/day2/ScoreDemo3.java
@@ -0,0 +1,20 @@
+package day2;
+
+public class ScoreDemo3 {
+
+	public static void main(String[] args) {
+		// 利用陣列來儲存 5 個學生物件
+		Student[] students = {
+				new Student("John", 95),
+				new Student("Mary", 50),
+				new Student("Alen", 83),
+				new Student("Jack", 40),
+				new Student("Rose", 60)
+		};
+		
+		
+		
+
+	}
+
+}
