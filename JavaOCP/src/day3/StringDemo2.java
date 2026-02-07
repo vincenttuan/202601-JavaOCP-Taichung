9219ddb143bf4c674d9e9f63acebdae38b6d1543
FP <FP@FP>
2026-02-07 12:12:27 +0800

    String

---
+++ JavaOCP/src/day3/StringDemo2.java
@@ -0,0 +1,20 @@
+package day3;
+
+public class StringDemo2 {
+
+	public static void main(String[] args) {
+		String s1 = new String("Java");
+		String s2 = new String("Java");
+		String s3 = "Java";
+		String s4 = "Java";
+		
+		System.out.printf("s1 == s2 %b%n", s1 == s2);
+		System.out.printf("s3 == s4 %b%n", s3 == s4);
+		System.out.printf("s1.equals(s2) %b%n", s1.equals(s2));
+		System.out.printf("s3.equals(s4) %b%n", s3.equals(s4));
+		System.out.printf("s1.equals(s3) %b%n", s1.equals(s3));
+		
+
+	}
+
+}
