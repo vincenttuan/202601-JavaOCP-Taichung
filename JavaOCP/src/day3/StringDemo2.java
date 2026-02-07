d79af55173f37a87173b3b5fd71663cfc3c4e903
FP <FP@FP>
2026-02-07 12:16:29 +0800

    String

--- JavaOCP/src/day3/StringDemo2.java
+++ JavaOCP/src/day3/StringDemo2.java
@@ -7,12 +7,18 @@ public class StringDemo2 {
 		String s2 = new String("Java");
 		String s3 = "Java";
 		String s4 = "Java";
+		String s5 = "  Java ";
 		
 		System.out.printf("s1 == s2 %b%n", s1 == s2);
 		System.out.printf("s3 == s4 %b%n", s3 == s4);
 		System.out.printf("s1.equals(s2) %b%n", s1.equals(s2));
 		System.out.printf("s3.equals(s4) %b%n", s3.equals(s4));
 		System.out.printf("s1.equals(s3) %b%n", s1.equals(s3));
+		System.out.printf("s4.equals(s5) %b%n", s4.equals(s5));
+		System.out.printf("s1.equals(s5) %b%n", s1.equals(s5));
+		// trim() 去除左右空白
+		System.out.printf("去空白後比較 s1.equals(s5.trim()) %b%n", s1.equals(s5.trim()));
+		
 		
 
 	}
