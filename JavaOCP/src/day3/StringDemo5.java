3c34a2260a6f892f4b52cf73e28b70941f90f351
FP <FP@FP>
2026-02-07 13:58:50 +0800

    split

---
+++ JavaOCP/src/day3/StringDemo5.java
@@ -0,0 +1,19 @@
+package day3;
+
+public class StringDemo5 {
+
+	public static void main(String[] args) {
+		String name = "Vincent,Jo,Anita,Howard,Alen,Rose,Bob";
+		// 1.請問有幾組人名 ?
+		
+		// 2.平均每個人名是幾個字 ?
+		
+		// 3.最長的名字是 ?
+		
+		// 4.最短的名字是 ?
+		
+		// 5.名字由短 -> 長排序
+		
+	}
+
+}
