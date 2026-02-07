bafe893ed1486003b93a17e417bdb691aa7b60c6
FP <FP@FP>
2026-02-07 12:28:20 +0800

    String

--- JavaOCP/src/day3/StringDemo3.java
+++ JavaOCP/src/day3/StringDemo3.java
@@ -12,12 +12,15 @@ public class StringDemo3 {
 		String menu = "雞腿飯,排骨飯,魚排飯,蝦捲飯,牛肉炒飯,羊肉炒麵";
 		// menu 有包含"排骨飯"嗎 ?
 		System.out.printf("有排骨飯嗎 ? %b%n", menu.contains("排骨飯"));
+		System.out.printf("有排骨飯嗎 ? %s%n", (menu.contains("排骨飯") ? "有" : "沒有"));
 		
 		// menu 有包含"豬腳飯"嗎 ?
 		System.out.printf("有豬腳飯嗎 ? %b%n", menu.contains("豬腳飯"));
+		System.out.printf("有豬腳飯嗎 ? %s%n", (menu.contains("豬腳飯") ? "有" : "沒有"));
 		
 		// menu 有包含"炒麵"嗎 ?
 		System.out.printf("有炒麵嗎 ? %b%n", menu.contains("炒麵"));
+		System.out.printf("有炒麵嗎 ? %s%n", (menu.contains("炒麵") ? "有" : "沒有"));
 	}
 
 }
