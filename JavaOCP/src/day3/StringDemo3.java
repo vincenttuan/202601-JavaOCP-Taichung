8164ebebc81a2bdee9fc22ad2c4bcdfd10cd80fc
FP <FP@FP>
2026-02-07 12:25:58 +0800

    String

---
+++ JavaOCP/src/day3/StringDemo3.java
@@ -0,0 +1,23 @@
+package day3;
+
+public class StringDemo3 {
+
+	public static void main(String[] args) {
+		// 陣列長度 length
+		// 字串長度 length()
+		String name = "John";
+		System.out.printf("%s 的長度 = %d%n", name, name.length());
+		
+		// 包含 contain
+		String menu = "雞腿飯,排骨飯,魚排飯,蝦捲飯,牛肉炒飯,羊肉炒麵";
+		// menu 有包含"排骨飯"嗎 ?
+		System.out.printf("有排骨飯嗎 ? %b%n", menu.contains("排骨飯"));
+		
+		// menu 有包含"豬腳飯"嗎 ?
+		System.out.printf("有豬腳飯嗎 ? %b%n", menu.contains("豬腳飯"));
+		
+		// menu 有包含"炒麵"嗎 ?
+		System.out.printf("有炒麵嗎 ? %b%n", menu.contains("炒麵"));
+	}
+
+}
