7372e687e776a7d2bc1c07f69fd67261fcaf71f9
FP <FP@FP>
2026-02-07 10:01:20 +0800

    two sum

---
+++ JavaOCP/src/day3/TwoSum.java
@@ -0,0 +1,24 @@
+package day3;
+
+import java.util.Arrays;
+
+// LeetCode 題目:二數之和(Two Sum)
+public class TwoSum {
+	
+	public static void main(String[] args) {
+		int[] nums = {2, 7, 11, 15};
+		int target = 18;
+		int[] result = new int[2]; // 預設值 = {0, 0}
+		int lens = nums.length; // 4
+		// 印出初始資料
+		System.out.println("印出初始資料");
+		System.out.printf("nums = %s%n", Arrays.toString(nums));
+		System.out.printf("result = %s%n", Arrays.toString(result));
+		
+		// 暴力比對
+		
+		
+
+	}
+
+}
