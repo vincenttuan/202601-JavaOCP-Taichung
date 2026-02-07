e0502f23aaf809009e8f2a7eda147c316b81ab6f
FP <FP@FP>
2026-02-07 10:13:37 +0800

    two sum

--- JavaOCP/src/day3/TwoSum.java
+++ JavaOCP/src/day3/TwoSum.java
@@ -7,7 +7,7 @@ public class TwoSum {
 	
 	public static void main(String[] args) {
 		int[] nums = {2, 7, 11, 15};
-		int target = 18;
+		int target = 13;
 		int[] result = new int[2]; // 預設值 = {0, 0}
 		int lens = nums.length; // 4
 		// 印出初始資料
@@ -16,8 +16,22 @@ public class TwoSum {
 		System.out.printf("result = %s%n", Arrays.toString(result));
 		
 		// 暴力比對
-		
-		
+		for(int i=0 ; i<(lens-1) ; i++) {
+			
+			for(int k=i+1 ; k<lens ; k++) {
+				int sum = nums[i] + nums[k];
+				System.out.printf("nums[%d][%d] = %d%n", i, k, sum);
+				// 判斷
+				if(sum == target) {
+					result[0] = i;
+					result[1] = k;
+				}
+			}
+			
+		}
+		System.out.println("\n印出結果資料");
+		System.out.printf("target = %d%n", target);
+		System.out.printf("result = %s%n", Arrays.toString(result));
 
 	}
 
