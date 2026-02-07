9ab07550a2325980da6729e386166b4fd95d4e99
FP <FP@FP>
2026-02-07 11:01:13 +0800

    bs

---
+++ JavaOCP/src/day3/BubbleSort.java
@@ -0,0 +1,26 @@
+package day3;
+
+import java.util.Arrays;
+
+// 泡沫排序法
+public class BubbleSort {
+
+	public static void main(String[] args) {
+		int[] nums = {5, 3, 8, 4, 2};
+		int lens = nums.length; // 5
+		System.out.printf("原始陣列: %s%n", Arrays.toString(nums));
+		
+		for(int i=0 ; i<lens-1 ; i++) {
+			
+			if(nums[i] > nums[i+1]) {
+				int tmp = nums[i];
+				nums[i] = nums[i+1];
+				nums[i+1] = tmp;
+			}
+			
+			System.out.printf("過程陣列: %s%n", Arrays.toString(nums));
+		}
+
+	}
+
+}
