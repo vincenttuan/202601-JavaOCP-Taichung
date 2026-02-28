aa73498032bdc85bb56bbbe742ac59fb8c3844ba
FP <FP@FP>
2026-02-28 14:54:31 +0800

    java

--- JavaOCP/src/day4/movie/FilmCatalog.java
+++ JavaOCP/src/day4/movie/FilmCatalog.java
@@ -8,18 +8,16 @@ public class FilmCatalog {
 	private static final Movie GIRLS = new Movie("陽光女子合唱團", "2hr10m");
 	
 	// 定義電影廳
-	private static final Hall[] HALLS = {
-			new Hall(TEAMS, "A", "iMax"),  // HALLS[0]
-			new Hall(TEAMS, "B", "3D"),    // HALLS[1]
-			new Hall(GIRLS, "C", "普通")    // HALLS[2]
-	};
+	private static final Hall A = new Hall(TEAMS, "A", "iMax");
+	private static final Hall B = new Hall(TEAMS, "B", "3D");
+	private static final Hall C = new Hall(GIRLS, "C", "普通");
 	
 	// 定義可訂座位
 	private static final Seat[] SEATS = {
-			new Seat(HALLS[0], 1, 250), //0: A廳(放映復仇者聯盟4) 1排 $250元
-			new Seat(HALLS[0], 2, 240), //1: A廳(放映復仇者聯盟4) 2排 $240元
-			new Seat(HALLS[1], 3, 220), //2: B廳(放映復仇者聯盟4) 3排 $220元
-			new Seat(HALLS[2], 2, 260), //3: C廳(陽光女子合唱團) 2排 $260元
+			new Seat(A, 1, 250), //0: A廳(放映復仇者聯盟4) 1排 $250元
+			new Seat(A, 2, 240), //1: A廳(放映復仇者聯盟4) 2排 $240元
+			new Seat(B, 3, 220), //2: B廳(放映復仇者聯盟4) 3排 $220元
+			new Seat(C, 2, 260), //3: C廳(陽光女子合唱團)  2排 $260元
 	};
 	
 	// 根據 index 找到可訂座位
