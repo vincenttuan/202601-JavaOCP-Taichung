f48964aa38d0ddef3b698f7225bb6b153bef9282
FP <FP@FP>
2026-02-28 15:59:09 +0800

    java

---
+++ JavaOCP/src/day4/Account.java
@@ -0,0 +1,49 @@
+package day4;
+
+// 銀行帳號
+public class Account {
+	
+	private String number; // 帳號
+	private String name; // 戶名
+	private int balance; // 存款金額
+	
+	// 建構子(建立帳號物件)
+	public Account(String number, String name, int balance) {
+		this.number = number;
+		this.name = name;
+		this.balance = balance;
+	}
+	
+	// 封裝方法 getters
+	public String getNumber() {
+		return number;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getBalance() {
+		return balance;
+	}
+	
+	// 商業邏輯方法(存/提/轉帳)
+	public void deposit(int amount) {
+		balance += amount;
+	}
+	
+	public void withdraw(int amount) {
+		balance -= amount;
+	}
+	
+	public void transfer(int amount, Account other) {
+		this.withdraw(amount); // 提款
+		other.deposit(amount); // 存款
+	}
+	
+	// 將物件內的資料以字串回傳
+	public String toString() {
+		return String.format("帳號:%s 戶名:%s 存款:%d", number, name, balance);
+	}
+	
+}
