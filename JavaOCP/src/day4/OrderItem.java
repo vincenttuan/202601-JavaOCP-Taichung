a7b448715a2c5e5b3088d6b30f0cafdefe321220
FP <FP@FP>
2026-02-28 10:24:37 +0800

    java

--- JavaOCP/src/day4/OrderItem.java
+++ JavaOCP/src/day4/OrderItem.java
@@ -5,7 +5,7 @@ public class OrderItem {
 	
 	// 物件屬性
 	private Drink drink; // 飲料物件
-	public int quantity; // 訂購數量
+	private int quantity; // 訂購數量
 	
 	// 建立一個建構子來設定屬性資料
 	public OrderItem(Drink drink, int quantity) {
@@ -13,10 +13,19 @@ public class OrderItem {
 		this.quantity = quantity;
 	}
 	
+	// getter
 	// getTotalPrice()：回傳這筆訂單小計
 	public int getTotalPrice() {
 		// 飲料的價格 * 訂購數量 => drink 的 price * quantity
 		return drink.getPrice() * this.quantity;
+	}
+	
+	public Drink getDrink() {
+		return drink;
+	}
+	
+	public int getQuantity() {
+		return quantity;
 	}
 	
 }
---
+++ JavaOCP/test/day4/TestOrderItem.java
@@ -0,0 +1,21 @@
+package day4;
+
+public class TestOrderItem {
+
+	public static void main(String[] args) {
+		System.out.println("測試資料");
+		System.out.println("=================");
+		
+		// 建立二杯飲料
+		Drink d1 = new Drink(1, "珍珠奶茶", 60);
+		Drink d2 = new Drink(2, "甘蔗清茶", 50);
+		
+		// 建立訂單項目
+		OrderItem oItem1 = new OrderItem(d1, 1); // 珍珠奶茶 1杯
+		OrderItem oItem2 = new OrderItem(d2, 3); // 甘蔗清茶 3杯
+		
+		// 印出訂單項目資訊
+		System.out.println("");
+	}
+
+}
