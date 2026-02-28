3ff826e58710dff1d3c429da9f659aa9ddf27ee4
FP <FP@FP>
2026-02-28 10:03:31 +0800

    java

---
+++ JavaOCP/test/day4/TestDrink.java
@@ -0,0 +1,18 @@
+package day4;
+
+public class TestDrink {
+
+	public static void main(String[] args) {
+		System.out.println("測試資料");
+		System.out.println("=================");
+		
+		// 建立二杯飲料
+		Drink d1 = new Drink(1, "珍珠奶茶", 60);
+		Drink d2 = new Drink(2, "甘蔗清茶", 50);
+		
+		// 印出資料
+		System.out.println(d1.getDisplayName());
+		System.out.println(d2.getDisplayName());
+	}
+
+}
