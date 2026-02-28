0e8aa73d1f5a51e3ef6aee643a32d5470fca5e6b
FP <FP@FP>
2026-02-28 12:02:28 +0800

    java

--- JavaOCP/test/day4/TestSplit.java
+++ JavaOCP/test/day4/TestSplit.java
@@ -30,6 +30,17 @@ public class TestSplit {
 			}
 			// 印出結果
 			System.out.printf("%d號x%d杯%n", number, quantity);
+			
+			// 找飲料並建立訂單項目
+			Drink drink = DrinkMenu.findByNumber(number);
+			OrderItem oItem = new OrderItem(drink, quantity);
+			// 列印訂單資料
+			System.out.printf("品名:%s 單價:%d 數量:%d 小計:%d%n", 
+					  oItem.getDrink().getName(),
+					  oItem.getDrink().getPrice(),
+					  oItem.getQuantity(),
+					  oItem.getTotalPrice());
+			System.out.println(); 
 		}
 
 	}
