b6f68e73cb917c5a663c4032615d36f358c5661e
FP <FP@FP>
2026-02-28 14:18:32 +0800

    java

--- JavaOCP/src/day4/SeatOrder.java
+++ JavaOCP/src/day4/SeatOrder.java
@@ -1,5 +1,28 @@
 package day4;
 
+// 訂位
 public class SeatOrder {
+	
+	// 物件屬性
+	private Seat seat; // 預定位子
+	private int quantity; // 張數
+	
+	public SeatOrder(Seat seat, int quantity) {
+		super();
+		this.seat = seat;
+		this.quantity = quantity;
+	}
 
+	public Seat getSeat() {
+		return seat;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+	
+	public int getTotalPrice() {
+		return seat.getPrice() * quantity; // 座位價格 x 張數
+	}
+	
 }
