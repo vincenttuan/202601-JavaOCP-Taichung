e146de91eee8f44e726930c37b97157bb6a543a6
FP <FP@FP>
2026-02-28 09:56:30 +0800

    java

---
+++ JavaOCP/src/day4/Drink.java
@@ -0,0 +1,24 @@
+package day4;
+
+// 飲料基本資料
+public class Drink {
+	// 物件屬性
+	private int number; // 編號
+	private String name; // 名稱
+	private int price; // 價格
+	
+	// 建立一個建構子來設定屬性資料
+	public Drink(int number, String name, int price) {
+		this.number = number;
+		this.name = name;
+		this.price = price;
+	}
+	
+	// getter
+	public String getDisplayName() {
+		//return number + "." + name + " $" + price;
+		return String.format("%d.%s $%d", number, name, price);
+		
+	}
+	
+}
