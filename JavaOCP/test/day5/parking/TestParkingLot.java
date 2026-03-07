f9e061083140928bbd6e2f14b678548e89c665e3
FP <FP@FP>
2026-03-07 14:45:48 +0800

    test

--- JavaOCP/test/day5/parking/TestParkingLot.java
+++ JavaOCP/test/day5/parking/TestParkingLot.java
@@ -9,10 +9,11 @@ public class TestParkingLot {
 
 		// 建立車
 		Car car = new Car(4, "1234-AB");
+		Bike bike = new Bike(false, "5678-XY");
 		
 		// 停車
 		lot.parking(car, 2);
-		
+		lot.parking(bike, 1);
 
 	}
 
