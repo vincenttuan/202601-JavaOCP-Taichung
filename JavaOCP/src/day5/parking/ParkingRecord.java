4cff9920d520ec74ba3d343d2660e7d26737e88a
FP <FP@FP>
2026-03-07 14:06:11 +0800

    test

--- JavaOCP/src/day5/parking/ParkingRecord.java
+++ JavaOCP/src/day5/parking/ParkingRecord.java
@@ -48,6 +48,30 @@ public class ParkingRecord {
 		this.ratePerSecond = ratePerSecond;
 	}
 	
+	// 商業邏輯方法
+	// 計算停車秒數
+	public int getParkingSeconds() {
+		long now = System.currentTimeMillis(); // 取得現在時刻
+		long diff = now - entryTime; // 現在時刻 - 停車時刻 (單位 ms)
+		return (int)diff / 1000; // 因為 1s = 1000ms
+	}
+	
+	// 取得並計算停車費
+	public int getFee() {
+		int fee = getParkingSeconds() * ratePerSecond;
+		return fee;
+	}
+	
+	// 顯示停車資訊
+	public void showInfo() {
+		System.out.printf("車牌：%s 停車格：%d 停車秒數：%d 秒 停車費：%d 元%n",
+				this.getVehicle().getPlateNumber(),
+				this.getSpaceNumber(),
+				this.getParkingSeconds(),
+				this.getFee());
+	}
+	
+	// 複寫方法
 	public String toString() {
 		SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
 		return String.format("車牌:%s 車位:%d 進場時間:%s 費率:%d/秒", 
