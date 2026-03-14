49fc899624f42964b9ececef5458fd47d31c390d
FP <FP@FP>
2026-03-14 10:40:27 +0800

    java

--- JavaOCP/src/day6/Employee.java
+++ JavaOCP/src/day6/Employee.java
@@ -1,14 +1,25 @@
 package day6;
 
 public class Employee {
+	// 薪資
+	public int getSalary() {
+		return 45000;
+	}
 	
 }
 
-class Manager {
+class Manager extends Employee {
+	// 預算
+	public int getBudget() {
+		return 10_0000;
+	}
 	
 }
 
-class Boss {
-	
+class Boss extends Employee {
+	// 股票資產
+	public int getStock() {
+		return 500_0000;
+	}
 }
 
