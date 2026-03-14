a02993fa0b58d38a2c6661a5a6655bec9b04b35b
FP <FP@FP>
2026-03-14 10:06:29 +0800

    java

---
+++ JavaOCP/test/day6/TestBeverage.java
@@ -0,0 +1,10 @@
+package day6;
+
+public class TestBeverage {
+	public static void main(String[] args) {
+		// 飲料測試
+		Beverage beverage1 = new Beverage();
+		System.out.printf("飲料名: %s%n", beverage1.getName());
+		
+	}
+}
