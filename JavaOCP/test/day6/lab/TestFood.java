13c435f7fb9e3fc71b2cba85391b9c53d08179b3
FP <FP@FP>
2026-03-14 12:26:22 +0800

    java

---
+++ JavaOCP/test/day6/lab/TestFood.java
@@ -0,0 +1,24 @@
+package day6.lab;
+
+public class TestFood {
+
+	public static void main(String[] args) {
+		System.out.println("NightMarketFood 測試 =====");
+		Food night = new NightMarketFood();
+		night.order();
+		night.order("蚵仔煎");
+		night.order("蛋炒飯", 2);
+		night.prepare();
+		night.serve();
+		
+		System.out.println("\nHotPotFood 測試 =====");
+		Food hotpot = new HotPotFood();
+		hotpot.order();
+		hotpot.order("羊肉片");
+		hotpot.order("臭豆腐", 3);
+		hotpot.prepare();
+		hotpot.serve();
+		
+	}
+
+}
