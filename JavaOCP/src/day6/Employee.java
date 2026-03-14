b537f0d58cb07b32f3cd9ec7b41a29dbd15134f9
FP <FP@FP>
2026-03-14 10:55:24 +0800

    java

--- JavaOCP/src/day6/Employee.java
+++ JavaOCP/src/day6/Employee.java
@@ -13,13 +13,28 @@ class Manager extends Employee {
 	public int getBudget() {
 		return 10_0000;
 	}
-	
+
+	@Override
+	public int getSalary() {
+		return 80_0000;
+	}
 }
 
-class Boss extends Employee {
+class Boss extends Manager {
 	// 股票資產
 	public int getStock() {
 		return 500_0000;
 	}
+
+	@Override
+	public int getBudget() {
+		return 200_0000;
+	}
+
+	@Override
+	public int getSalary() {
+		return 1;
+	}
+	
 }
 
--- JavaOCP/test/day6/TestEmployee.java
+++ JavaOCP/test/day6/TestEmployee.java
@@ -4,6 +4,7 @@ public class TestEmployee {
 
 	public static void main(String[] args) {
 		Employee emp1 = new Manager();
+		System.out.println(emp1.getClass().getSimpleName());
 		System.out.println(emp1.getSalary());
 		// 轉型前判斷
 		if(emp1 instanceof Manager) {
