d953b320dcd1b7e012402ff5fda78e0a85002887
FP <FP@FP>
2026-03-14 12:19:51 +0800

    java

--- JavaOCP/src/day6/lab/HotPotFood.java
+++ JavaOCP/src/day6/lab/HotPotFood.java
@@ -1,5 +1,13 @@
 package day6.lab;
 
 public class HotPotFood extends Food {
-
+	@Override
+	public void prepare() {
+		System.out.println("開火-煮水");
+	}
+	
+	@Override
+	public void order() {
+		System.out.println("麻辣鴨血一份");
+	}
 }
