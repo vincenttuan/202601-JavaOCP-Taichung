cd461153d8e069a990ad4f968f88e634d0ed1eeb
FP <FP@FP>
2026-03-14 12:18:30 +0800

    food

--- JavaOCP/src/day6/lab/NightMarketFood.java
+++ JavaOCP/src/day6/lab/NightMarketFood.java
@@ -1,5 +1,15 @@
 package day6.lab;
 
 public class NightMarketFood extends Food {
-
+	
+	@Override
+	public void prepare() {
+		System.out.println("開火-熱鍋冷油");
+	}
+	
+	@Override
+	public void order() {
+		System.out.println("雞排一份");
+	}
+	
 }
