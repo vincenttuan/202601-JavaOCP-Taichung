69df23237e1f4f21e3453c0d175fc3de1dbeff76
FP <FP@FP>
2026-03-14 10:49:57 +0800

    java

--- JavaOCP/test/day6/TestEmployee.java
+++ JavaOCP/test/day6/TestEmployee.java
@@ -5,8 +5,14 @@ public class TestEmployee {
 	public static void main(String[] args) {
 		Employee emp1 = new Manager();
 		System.out.println(emp1.getSalary());
-		System.out.println(((Manager)emp1).getBudget());
-
+		// 轉型前判斷
+		if(emp1 instanceof Manager) {
+			System.out.println(((Manager)emp1).getBudget());
+		}
+		// 轉型前判斷
+		if(emp1 instanceof Boss) {
+			System.out.println(((Boss)emp1).getStock());
+		}
 	}
 
 }
