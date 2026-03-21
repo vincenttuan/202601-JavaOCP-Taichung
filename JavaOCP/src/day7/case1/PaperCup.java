0a50a295bdb74f71cf8adf46c573bbfba6ee1ac5
FP <FP@FP>
2026-03-21 10:31:40 +0800

    cup

--- JavaOCP/src/day7/case1/PaperCup.java
+++ JavaOCP/src/day7/case1/PaperCup.java
@@ -10,8 +10,8 @@ public class PaperCup implements Cup {
 	@Override
 	public void make() {
 		System.out.println("準備製作:" + name);
-		System.out.println("準備紙張");
 		System.out.println("杯子容量:" + Cup.capacity);
+		System.out.println("準備紙張:用紙壓制成型");
 		System.out.println("製作完成");
 	}
 
