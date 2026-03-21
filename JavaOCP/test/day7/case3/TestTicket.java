ce9ff5d4b0dd45ac3cb0980746e1988565ac71e6
FP <FP@FP>
2026-03-21 16:08:13 +0800

    case3

--- JavaOCP/test/day7/case3/TestTicket.java
+++ JavaOCP/test/day7/case3/TestTicket.java
@@ -4,10 +4,12 @@ public class TestTicket {
 
 	public static void main(String[] args) {
 		Ticket ticket = new MovieTicket(200, 5);
-		System.out.printf("票價: %.1f%n", ticket.getPrice());
-		System.out.printf("數量: %.1f%n", ticket.);
-		System.out.printf("訂票: %.1f%n", ticket.book());
-
+		System.out.printf("票價: %,.1f%n", ticket.getPrice());
+		System.out.printf("數量: %,.1f%n", ((MovieTicket)ticket).getQty());
+		System.out.printf("手續費: %,.1f%n", ticket.calculateFee());
+		System.out.printf("訂票: %,.1f%n", ticket.book());
+		System.out.println("===========");
+		System.out.printf("退票: %,.1f%n", ticket.refund());
 	}
 
 }
