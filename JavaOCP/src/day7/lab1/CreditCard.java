647295b0bb0d6b48bad39c67e8ad9b7cd7fa9d20
FP <FP@FP>
2026-03-21 14:03:49 +0800

    lab

---
+++ JavaOCP/src/day7/lab1/CreditCard.java
@@ -0,0 +1,23 @@
+package day7.lab1;
+
+public class CreditCard extends AbstractPayment {
+	final double RATE = 0.03; // 手續費率
+	
+	@Override
+	public void pay(double amount) {
+		double fee = calaulateFee(amount); // 手續費
+		System.out.printf("信用卡付款: %.1f 手續費: %.1f%n", amount, fee);
+		
+	}
+
+	@Override
+	public void refund(double amount) {
+		System.out.printf("信用卡退款: %.1f%n", amount);
+	}
+
+	@Override
+	public double calaulateFee(double amount) {
+		return amount * RATE;
+	}
+
+}
