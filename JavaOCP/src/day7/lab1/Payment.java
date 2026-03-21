d3f4b8b0ae7f3aaa4be6ef31119c3a90e7c5ad9f
FP <FP@FP>
2026-03-21 13:56:12 +0800

    lab

--- JavaOCP/src/day7/lab1/Payment.java
+++ JavaOCP/src/day7/lab1/Payment.java
@@ -1,5 +1,22 @@
 package day7.lab1;
 
+/** 
+ *                 +---------+
+ *                 | Payment |  <interface>
+ *                 +---------+
+ *                      ^
+ *                      |
+ *             +-----------------+
+ *             | AbstractPayment |  <abstract class>
+ *             +-----------------+
+ *              ^       ^       ^   
+ *              |       |       | 
+ * +------------+  +---------+  +----------+
+ * | CreditCard |  | LinePay |  | ApplePay |
+ * +------------+  +---------+  +----------+
+ * */
+
+
 public interface Payment {
 	void pay(double amount); // 付款
 	void refund(double amount); // 退款
