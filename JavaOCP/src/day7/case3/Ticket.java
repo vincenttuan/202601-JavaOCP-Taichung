97b98aca2394c59465e2998d57a6574553632ea8
FP <FP@FP>
2026-03-21 16:16:26 +0800

    case3

--- JavaOCP/src/day7/case3/Ticket.java
+++ JavaOCP/src/day7/case3/Ticket.java
@@ -4,4 +4,15 @@ package day7.case3;
 public interface Ticket extends Bookable, Refundable, FeeCalculate {
 	double getPrice();
 	int getQty();
+	
+	// java 8 透過 default 關鍵子可以在 interface 中提供有實作的方法
+	default void printInfo() {
+		System.out.printf("票價: %,.1f%n", getPrice());
+		System.out.printf("數量: %d%n", getQty());
+		System.out.printf("手續費: %,.1f%n", calculateFee());
+		System.out.printf("訂票: %,.1f%n", book());
+		System.out.println("===========");
+		System.out.printf("退票: %,.1f%n", refund());
+	}
+	
 }
