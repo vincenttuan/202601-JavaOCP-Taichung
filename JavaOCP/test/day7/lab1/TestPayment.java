2dc228bc4491234f7e1d4d6c53034cf9b6c17b5d
FP <FP@FP>
2026-03-21 14:12:08 +0800

    lab

--- JavaOCP/test/day7/lab1/TestPayment.java
+++ JavaOCP/test/day7/lab1/TestPayment.java
@@ -5,8 +5,11 @@ public class TestPayment {
 	public static void main(String[] args) {
 		Payment payment1 = new CreditCard();
 		// 付款
+		System.out.println("=== 付款測試 ===");
 		payment1.pay(10000);
+		
 		// 退款
+		System.out.println("\n=== 退款測試 ===");
 		payment1.refund(10000);
 	}
 	
