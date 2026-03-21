68b9817bb020ed55b471909996b3307a02da51b5
FP <FP@FP>
2026-03-21 14:59:12 +0800

    lab2

--- JavaOCP/test/day7/lab2/TestTicket.java
+++ JavaOCP/test/day7/lab2/TestTicket.java
@@ -7,12 +7,19 @@ public class TestTicket {
 		Ticket normal = new NormalTicket("一般票", 2000);
 		Ticket student = new StudentTicket("學生票", 1000);
 		
-		// 假設 qty=2
+		// 假設數量=2
 		int qty = 2;
 		
+		System.out.println("=== 訂票測試 ===");
 		System.out.printf("VIP訂票: $%,.1f%n", vip.book(qty));
 		System.out.printf("一般訂票: $%,.1f%n", normal.book(qty));
 		System.out.printf("學生訂票: $%,.1f%n", student.book(qty));
+		
+		System.out.println("\n=== 退票測試 ===");
+		
+		System.out.printf("VIP退票: $%,.1f%n", vip.refund(qty));
+		System.out.printf("一般退票: $%,.1f%n", normal.refund(qty));
+		System.out.printf("學生退票: $%,.1f%n", student.refund(qty));
 		
 
 	}
