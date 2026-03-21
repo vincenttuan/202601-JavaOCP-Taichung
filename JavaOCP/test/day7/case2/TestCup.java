1206b28377840fff0f4530b8b73a73e29a3c6e08
FP <FP@FP>
2026-03-21 12:18:07 +0800

    cup

--- JavaOCP/test/day7/case2/TestCup.java
+++ JavaOCP/test/day7/case2/TestCup.java
@@ -4,8 +4,15 @@ public class TestCup {
 
 	public static void main(String[] args) {
 		Cup glassCup = new GlassCup("玻璃杯");
+		Cup steelCup = new SteelCup("不鏽鋼杯");
 		
-
+		System.out.println("=== 製作杯子 ===");
+		glassCup.make();
+		steelCup.make();
+		
+		System.out.println("\n=== 裝水測試 ===");
+		glassCup.fill();
+		steelCup.fill();
 	}
 
 }
