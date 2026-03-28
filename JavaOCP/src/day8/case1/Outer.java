e90d0508813056622f7b8f3742aaf83629a3ba07
FP <FP@FP>
2026-03-28 09:59:34 +0800

    java

--- JavaOCP/src/day8/case1/Outer.java
+++ JavaOCP/src/day8/case1/Outer.java
@@ -2,12 +2,17 @@ package day8.case1;
 
 // 外部類別
 public class Outer {
-	
+	public int x = 7; // Outer 的物件變數
 	// 內部類別
 	public class Inner {
-		
+		public int x = 77; // Inner 的物件變數
 		public void info() {
+			int x = 777; // info() 方法的區域變數
 			System.out.println("我是內部類別");
+			System.out.println(x); // 777
+			System.out.println(this.x); // 77
+			//System.out.println(Inner.this.x); // 77
+			System.out.println(Outer.this.x); // 7
 		}
 		
 	}
