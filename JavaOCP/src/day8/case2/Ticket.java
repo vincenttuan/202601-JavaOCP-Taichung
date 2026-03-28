61381cb217552e08d4da8b34ffe3eebd733c0280
FP <FP@FP>
2026-03-28 15:43:59 +0800

    java

--- JavaOCP/src/day8/case2/Ticket.java
+++ JavaOCP/src/day8/case2/Ticket.java
@@ -10,6 +10,29 @@ public class Ticket {
 		this.seat = seat;
 		this.price = price;
 	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		// 1. 自己比自己
+		if(this == obj) return true;
+		
+		// 2. null 檢查
+		if(obj == null) return false;
+		
+		// 3. 型別檢查
+		if(getClass() != obj.getClass()) return false;
+		
+		// 4. 轉型
+		Ticket other = (Ticket)obj;
+		
+		// 5. 比內容
+		// 相同 trainNo 與 seat 才視為相等
+		return this.trainNo == other.trainNo &&
+				this.seat.equals(other.seat);
+				
+	}
+
+
 
 	public int getTrainNo() {
 		return trainNo;
