95c26b746f43f4ab19ed48360ee7c84a537f4845
FP <FP@FP>
2026-03-28 10:54:02 +0800

    java

--- JavaOCP/src/day8/lab1/ShoppingApp.java
+++ JavaOCP/src/day8/lab1/ShoppingApp.java
@@ -3,13 +3,14 @@ package day8.lab1;
 /**
  * 設計一個電商系統(ShoppingApp)
  * 系統元件:
- * 使用者(User)
- * 折扣(Discount)
+ * 使用者(User) -> 一般內部類別
+ * 折扣(Discount) -> 方法內部類別
  * 付款(Payment)
  * 紀錄(Logger)
  * */
 public class ShoppingApp {
 	private String appName = "MyShop";
+	
 	// 讓 User 強烈依附在 ShoppingApp
 	public class User {
 		private String name;
@@ -20,4 +21,19 @@ public class ShoppingApp {
 			System.out.printf("%s 使用 %s%n", name, appName);
 		}
 	}
+	
+	// Discount 只在 checkout() 方法中使用
+	public void checkout(int total) {
+		class Discount {
+			int apply() {
+				return (int)(total * 0.9);
+			}
+		}
+		
+		int finalPrice = new Discount().apply();
+		System.out.printf("折扣前價格: %d%n", total);
+		System.out.printf("折扣後價格: %d%n", finalPrice);
+	}
+	
+	
 }
