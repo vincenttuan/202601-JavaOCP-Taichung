6fa7813010391eeb01cbbe3729c1980bdb6d4de0
FP <FP@FP>
2026-03-28 13:52:16 +0800

    java

---
+++ JavaOCP/test/day8/lab2/TestHighSpeedRailBooking.java
@@ -0,0 +1,16 @@
+package day8.lab2;
+
+public class TestHighSpeedRailBooking {
+	public static void main(String[] args) {
+		// 台中 -> 桃園
+		HighSpeedRailBooking train = new HighSpeedRailBooking("台中 -> 桃園");
+		
+		// 建立旅客
+		HighSpeedRailBooking.Passenger p1 = train.new Passenger("小明", "普通");
+		HighSpeedRailBooking.Passenger p2 = train.new Passenger("小華", "白金");
+		
+		// 顯示旅客資訊
+		p1.showInfo();
+		p2.showInfo();
+	}
+}
