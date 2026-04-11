a55e9c7547fd4ce824d38b4459615e25b6efbf24
FP <FP@FP>
2026-04-11 14:14:10 +0800

    set

--- JavaOCP/src/day9/SetDemo6.java
+++ JavaOCP/src/day9/SetDemo6.java
@@ -1,9 +1,26 @@
 package day9;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import day9.entity.Student;
+
 public class SetDemo6 {
 	public static void main(String[] args) {
 		// 集合單一元素: String, Integer ...
 		// 集合複合元素: 自訂物件
+		Set<Student> students = new HashSet<>();
+		students.add(new Student("Alice", 85, 2, 20));
+		students.add(new Student("Bob", 92, 1, 22));
+		students.add(new Student("Cindy", 78, 2, 19));
+		students.add(new Student("David", 88, 1, 21));
+		students.add(new Student("Eva", 95, 2, 23));
+		students.add(new Student("Frank", 67, 1, 20));
+		students.add(new Student("Grace", 73, 2, 22));
+		students.add(new Student("Henry", 81, 1, 24));
+		students.add(new Student("Ivy", 90, 2, 21));
+		students.add(new Student("Jack", 76, 1, 19));
+		
 		
 		
 	}
