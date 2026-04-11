1ce93a97412fc32c00894c1fcbdf814ed1d13c77
FP <FP@FP>
2026-04-11 10:46:09 +0800

    set

--- JavaOCP/src/day9/SetDemo1.java
+++ JavaOCP/src/day9/SetDemo1.java
@@ -9,18 +9,18 @@ public class SetDemo1 {
 
 	public static void main(String[] args) {
 		// 探討不同 Set 集合應用
-		// 將 [3] [8] [4] [5] 這四個元素依序放到 Set 集合中
+		// 將 [100] [2] [30] [1] 這四個元素依序放到 Set 集合中
 		
 		Set set1 = new HashSet();
-		set1.add(3);set1.add(8);set1.add(4);set1.add(5);
+		set1.add(100);set1.add(2);set1.add(30);set1.add(1);
 		
 		Set set2 = new LinkedHashSet();
-		set2.add(3);set2.add(8);set2.add(4);set2.add(5);
+		set2.add(100);set2.add(2);set2.add(30);set2.add(1);
 		
 		Set set3 = new TreeSet();
-		set3.add(3);set3.add(8);set3.add(4);set3.add(5);
+		set3.add(100);set3.add(2);set3.add(30);set3.add(1);
 		
-		System.out.println("將 [3] [8] [4] [5] 這四個元素依序放到 Set 集合中");
+		System.out.println("將 [100] [2] [30] [1] 這四個元素依序放到 Set 集合中");
 		System.out.println("HashSet(無序集合) => " + set1);
 		System.out.println("LinkedHashSet(有序集合) => " + set2);
 		System.out.println("TreeSet(排序集合) => " + set3);
