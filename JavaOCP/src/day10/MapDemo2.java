3bac411b6ae961c08b5a3bc64d6916619b52c215
FP <FP@FP>
2026-04-18 14:29:01 +0800

    map

--- JavaOCP/src/day10/MapDemo2.java
+++ JavaOCP/src/day10/MapDemo2.java
@@ -1,7 +1,9 @@
 package day10;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import day10.entity.Product;
 
@@ -19,7 +21,17 @@ public class MapDemo2 {
 			}
 		}
 		
+		// 利用 Map 特性快速查詢 -> 轉 Map (核心優化)
+		Map<Integer, Product> productMap = new HashMap<>();
+		for(Product product : products) {
+			productMap.put(product.getId(), product);
+		}
 		
+		// 查詢 103 商品 -> 速度極快 -> O(1)
+		Product p = productMap.get(103);
+		if(p != null) {
+			System.out.println(p.getName());
+		}
 		
 	}
 }
