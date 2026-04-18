1dbbfc135d99b126a7cb6c6e7dad975750f20a37
FP <FP@FP>
2026-04-18 15:59:24 +0800

    drink

--- JavaOCP/src/day10/DrinkDemo.java
+++ JavaOCP/src/day10/DrinkDemo.java
@@ -1,5 +1,13 @@
 package day10;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import day10.entity.Drink;
+import day10.entity.Order;
+
 public class DrinkDemo {
 
 	public static void main(String[] args) {
@@ -7,7 +15,29 @@ public class DrinkDemo {
 		// Set:有哪些飲料被買(不重複飲品)
 		// Map:每種飲品賣了幾杯
 		
+		// 飲品
+		Drink d1 = new Drink(1, "珍珠奶茶");
+		Drink d2 = new Drink(2, "香草拿鐵");
+		Drink d3 = new Drink(3, "香檳汽水");
 		
+		// 訂單
+		List<Order> orders = new ArrayList<>();
+		orders.add(new Order(1001, List.of(d1, d2)));
+		orders.add(new Order(1002, List.of(d1, d3)));
+		orders.add(new Order(1003, List.of(d2, d3)));
+		orders.add(new Order(1004, List.of(d1)));
+		
+		// Set 不重複飲品
+		Set<String> uniqueDrinks = new HashSet<>();
+		orders.forEach(order -> {
+			
+			order.getDrinks().forEach(drink -> {
+				uniqueDrinks.add(drink.getName());
+			});
+			
+		});
+		System.out.println("有哪些飲料被買(不重複飲品) => " + uniqueDrinks);
+									
 
 	}
 
