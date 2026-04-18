ebd44b83c373493da3201988a5669180e7f079c3
FP <FP@FP>
2026-04-18 10:19:09 +0800

    java

---
+++ JavaOCP/src/day10/ListDemo1.java
@@ -0,0 +1,32 @@
+package day10;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListDemo1 {
+	public static void main(String[] args) {
+		// ArrayList: 連續記憶體
+		// ArrayList: 查詢快, 插入刪除慢
+		// [100][80][70][80][90]
+		//   0   1   2   3   4
+		List<Integer> scores = new ArrayList<>();
+		scores.add(80);
+		scores.add(70);
+		scores.add(80);
+		scores.add(90);
+		scores.add(0, 100); // 指定插入
+		
+		System.out.println(scores);
+		System.out.println(scores.size());
+		System.out.println(scores.get(0)); // 100
+		
+		// [100][80][70][80][90]
+		//   0   1   2   3   4
+		scores.remove(0); // 移除 index = 0 的元素 (將資料搬走)
+		// [80][70][80][90]
+		//  0   1   2   3
+		System.out.println(scores);
+		
+		
+	}
+}
