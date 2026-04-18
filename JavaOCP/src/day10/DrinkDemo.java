20701615f2517ccf641a1d64b8fbe305caec961f
FP <FP@FP>
2026-04-18 15:42:33 +0800

    drink

---
+++ JavaOCP/src/day10/DrinkDemo.java
@@ -0,0 +1,14 @@
+package day10;
+
+public class DrinkDemo {
+
+	public static void main(String[] args) {
+		// List:所有訂單(每個人買甚麼)
+		// Set:有哪些飲料被買(不重複飲品)
+		// Map:每種飲品賣了幾杯
+		
+		
+
+	}
+
+}
---
+++ JavaOCP/src/day10/entity/Drink.java
@@ -0,0 +1,37 @@
+package day10.entity;
+
+public class Drink {
+	private Integer id;
+	private String name;
+	
+	public Drink() {
+		
+	}
+	
+	public Drink(Integer id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Drink [id=" + id + ", name=" + name + "]";
+	}
+	
+}
