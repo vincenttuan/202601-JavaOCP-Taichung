a3b1c39138f92fa58e74bb8debd54a6a0e7d1397
FP <FP@FP>
2026-04-25 13:52:48 +0800

    java

--- JavaOCP/src/day11/Lab1.java
+++ JavaOCP/src/day11/Lab1.java
@@ -1,5 +1,11 @@
 package day11;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 /**
  * 讀取 score.txt
  * 過濾 不是數字, 不在 0~100 間的資料
@@ -9,6 +15,42 @@ package day11;
 public class Lab1 {
 	
 	public static void main(String[] args) {
+		Path inputPath = Paths.get("src/day11/score.txt"); // 讀取路徑
+		Path outputPath = Paths.get("src/day11/result.txt"); // 寫入路徑
+		
+		try {
+			// 讀檔
+			List<String> lines = Files.readAllLines(inputPath);
+			
+			int sum = 0; // 總和
+			int count = 0; // 有效資料
+			// 走訪 + 過濾資料
+			for(String line : lines) {
+				try {
+					// 數字轉換檢查
+					int score = Integer.parseInt(line);
+					// 數字範圍檢查
+					if(score < 0 || score > 100) {
+						System.err.println(score + " 範圍錯誤");
+						continue;
+					}
+					count++; // 有效資料 + 1
+					sum += score; // 計算總和
+					System.out.println(score);
+				} catch (NumberFormatException e) {
+					System.err.println(line + " 無法轉成 int");
+				}
+			}
+			
+			double avg = sum / count;
+			System.out.println("有效:" + count);
+			System.out.println("總分:" + sum);
+			System.out.println("平均:" + avg);
+			
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
 	}
 	
