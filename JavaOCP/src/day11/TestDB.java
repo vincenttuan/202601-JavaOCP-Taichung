44f1be7168f391d2777e6ef4b7113f93db483d25
FP <FP@FP>
2026-04-25 16:15:29 +0800

    java

--- JavaOCP/src/day11/TestDB.java
+++ JavaOCP/src/day11/TestDB.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+// conn.close() 要記得撰寫 + try-catch
 public class TestDB {
 
 	public static void main(String[] args) {
