84058bd1a2da457dcf45c2697666c7c2e9db99ad
FP <FP@FP>
2026-04-25 10:31:09 +0800

    java

---
+++ JavaOCP/src/day11/Exception1.java
@@ -0,0 +1,25 @@
+package day11;
+
+// 在 java 中. try-catch 是用來處理例外(Exception)的機制
+// 避免程式在發生錯誤時直接崩潰
+public class Exception1 {
+
+	public static void main(String[] args) {
+		
+		try {
+			int a = 10;
+			int b = 0;
+			int result = a / b;
+			System.out.println(result);
+			
+		} catch (ArithmeticException e) {
+			System.out.println("發生錯誤");
+			System.out.println("錯誤原因:" + e);
+			System.out.println("錯誤原因:" + e.getMessage());
+		}
+		
+		System.out.println("程式可以繼續執行...");
+		
+	}
+
+}
