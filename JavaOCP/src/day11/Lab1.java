b6b3e315a8a7b30dd7f645d787afe794dac98f0d
FP <FP@FP>
2026-04-25 13:41:08 +0800

    java

---
+++ JavaOCP/src/day11/Lab1.java
@@ -0,0 +1,15 @@
+package day11;
+
+/**
+ * 讀取 score.txt
+ * 過濾 不是數字, 不在 0~100 間的資料
+ * 計算 有效筆數, 總分, 平均
+ * 結果寫入 result.txt
+ * */
+public class Lab1 {
+	
+	public static void main(String[] args) {
+		
+	}
+	
+}
