0ba1ba604a83ea2d65404e7bd198e3cf11f0dc28
FP <FP@FP>
2026-04-25 11:37:55 +0800

    java

---
+++ JavaOCP/src/day11/Exception5.java
@@ -0,0 +1,37 @@
+package day11;
+
+public class Exception5 {
+	
+	public static void main(String[] args) {
+		
+		try {
+			action();
+		} catch (ArithmeticException | ArrayIndexOutOfBoundsException e) {
+			System.out.println("數學或陣列錯誤");
+			System.out.println("錯誤原因:" + e.getMessage());
+			return; // 強迫離開
+		} catch (Exception e) {
+			System.out.println("其他錯誤");
+			System.out.println("錯誤原因:" + e.getMessage());
+			return; // 強迫離開
+		} finally {
+			// 將一定要執行的程式碼放在此區段中
+			// 不論是否有例外發生一定都會執行
+			System.out.println("程式結束");
+		}
+		
+	}
+	
+	public static void action() {
+		
+		int[] arr = {10, 0, 5, 2};
+		
+		int a = arr[0]; // 分子
+		int b = arr[4]; // 分母
+			
+		int result = a / b;
+			
+		System.out.println(result);
+		
+	}
+}
