9e7124d3d2ba1252d7404e5e91c472d2d192687a
FP <FP@FP>
2026-04-25 16:25:05 +0800

    java

--- JavaOCP/src/day11/TestDB2.java
+++ JavaOCP/src/day11/TestDB2.java
@@ -1,17 +1,26 @@
 package day11;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.List;
 
 // 透過 try(xxx) 來自動進行連線關閉
 // 語法名稱: try with resources 語法
 public class TestDB2 {
 
-	public static void main(String[] args) {
-		String url = "jdbc:mysql://localhost:3306/demo";
-		String user = "root";
-		String password = "12345678";
+	public static void main(String[] args) throws IOException {
+		//String url = "jdbc:mysql://localhost:3306/demo";
+		//String user = "root";
+		//String password = "12345678";
+		
+		List<String> lines = Files.readAllLines(Paths.get("src/day11/conn.txt"));
+		String url = lines.get(0);
+		String user = lines.get(1);
+		String password = lines.get(2);
 		
 		// 取得資料庫連線
 		try(Connection conn = DriverManager.getConnection(url, user, password);) {
