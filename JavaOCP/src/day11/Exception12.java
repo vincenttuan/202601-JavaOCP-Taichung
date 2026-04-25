af52aebecb40b51692394e9446f47eb21ef98044
FP <FP@FP>
2026-04-25 14:56:07 +0800

    java

---
+++ JavaOCP/src/day11/Exception12.java
@@ -0,0 +1,43 @@
+package day11;
+
+import java.util.Map;
+
+/**
+ * 寫一個登入系統
+ * 成功: true
+ * 失敗: 生成錯誤例外 + 錯誤原因 
+ * 採用 Checked Exception, 強迫要進行錯誤處理
+ * 建立自訂例外 LoginFailException
+ * 不想寫 try-catch 則需要外拋例外 throws XXXX
+ * */
+public class Exception12 {
+	
+	private static Map<String, String> userMap = Map.of("admin", "1234", "root", "12345678", "apple", "abc");
+	
+	public static void main(String[] args) throws LoginFailException {
+		boolean check1 = checkLogin("admin", "1234");
+		System.out.println("登入結果:" + check1);
+		//-----------------------------------------------------
+		boolean check2 = checkLogin("admin", "12345");
+		System.out.println("登入結果:" + check2);
+	}
+	
+	public static boolean checkLogin(String username, String password) throws LoginFailException {
+		// 1. 查 username
+		if (userMap.get(username) == null) {
+			//return false; // 查無使用者
+			throw new LoginFailException("查無使用者"); // 查無使用者
+		}
+		
+		// 2. 比對 password
+		if(!userMap.get(username).equals(password)) {
+			//return false; // 密碼不正確
+			throw new LoginFailException("密碼不正確"); // 密碼不正確
+		}
+		
+		// 3. 比對成功
+		return true;
+	}
+	
+
+}
