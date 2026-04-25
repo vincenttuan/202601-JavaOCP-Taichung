f8bd65a731f11b5378d1f0b48bfd932531ca08cc
FP <FP@FP>
2026-04-25 14:33:02 +0800

    java

--- JavaOCP/src/day11/Exception8.java
+++ JavaOCP/src/day11/Exception8.java
@@ -12,32 +12,23 @@ public class Exception8 {
 	private static Map<String, String> userMap = Map.of("admin", "1234", "root", "12345678", "apple", "abc");
 	
 	public static void main(String[] args) {
-		try {
-			boolean check1 = checkLogin("admin", "1234");
-			System.out.println("登入結果:" + check1);
-		} catch (RuntimeException e) {
-			System.err.println("登入失敗! 原因:" + e.getMessage());
-		}
-		//-----------------------------------------------------
-		try {
-			boolean check2 = checkLogin("admin", "12345");
-			System.out.println("登入結果:" + check2);
-		} catch (RuntimeException e) {
-			System.err.println("登入失敗! 原因:" + e.getMessage());
-		}
+		boolean check1 = checkLogin("admin", "1234");
+		System.out.println("登入結果:" + check1);
+		
+		boolean check2 = checkLogin("admin", "12345");
+		System.out.println("登入結果:" + check2);
+		
 	}
 	
 	public static boolean checkLogin(String username, String password) {
 		// 1. 查 username
 		if (userMap.get(username) == null) {
-			//return false; // 查無使用者
-			throw new RuntimeException("查無使用者"); // 查無使用者
+			return false; // 查無使用者
 		}
 		
 		// 2. 比對 password
 		if(!userMap.get(username).equals(password)) {
-			//return false; // 密碼不正確
-			throw new RuntimeException("密碼不正確"); // 密碼不正確
+			return false; // 密碼不正確
 		}
 		
 		// 3. 比對成功
