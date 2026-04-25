88e94f3bc130848eb660ad5e2bd2208a51e0e178
FP <FP@FP>
2026-04-25 10:48:36 +0800

    java

---
+++ JavaOCP/src/day11/Exception3.java
@@ -0,0 +1,30 @@
+package day11;
+
+public class Exception3 {
+
+	public static void main(String[] args) {
+		
+		int[] arr = {10, 0, 5, 2};
+		
+		try {
+			
+			int a = arr[0]; // 分子
+			int b = arr[4]; // 分母
+			
+			int result = a / b;
+			
+			System.out.println(result);
+			
+		} catch (ArithmeticException e) {
+			System.out.println("數學錯誤");
+			System.out.println("錯誤原因:" + e.getMessage());
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("陣列錯誤");
+			System.out.println("錯誤原因:" + e.getMessage());
+		} catch (Exception e) {
+			System.out.println("其他錯誤");
+			System.out.println("錯誤原因:" + e.getMessage());
+		}
+		
+	}
+}
