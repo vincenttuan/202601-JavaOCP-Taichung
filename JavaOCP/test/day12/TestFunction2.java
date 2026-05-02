2a5e7d99bbb952562ec784ee31b781eee9eceaa1
FP <FP@FP>
2026-05-02 14:41:04 +0800

    function

--- JavaOCP/test/day12/TestFunction2.java
+++ JavaOCP/test/day12/TestFunction2.java
@@ -1,5 +1,9 @@
 package day12;
 
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.function.UnaryOperator;
+
 /*
  * 1個參數 + 1個回傳
 @FunctionalInterface
@@ -30,8 +34,22 @@ public interface BinaryOperator<T> extends BiFunction<T,T,T> {
 public class TestFunction2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// 計算圓面積
+		Function<Double, Double> func1 = (r) -> r * r * 3.1415926;
+		UnaryOperator<Double>    func2 = (r) -> r * r * 3.1415926; 
+		
+		double r = 10.0; // 半徑
+		System.out.printf("半徑: %.1f 圓面積: %.1f%n", r, func1.apply(r));
+		System.out.printf("半徑: %.1f 圓面積: %.1f%n", r, func2.apply(r));
+		
+		// 計算 bmi
+		// 輸入:身高:170.0(Double) 體重:60.0(Double) 回傳:bmi:20.76(Double)
+		BiFunction<Double, Double, Double> bmi1 = (h, w) -> w / Math.pow(h/100, 2);
+		
+		double h = 170.0;
+		double w = 60.0;
+		System.out.printf("身高: %.1f 體重: %.1f bmi: %.2f%n", h, w, bmi1.apply(h, w));
+		
 	}
 
 }
