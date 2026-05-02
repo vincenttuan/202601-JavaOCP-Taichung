0b1a4df8fd3abe9e9c74bc7e9f944e5c2a0c156e
FP <FP@FP>
2026-05-02 11:03:42 +0800

    java

--- JavaOCP/src/day12/Calc.java
+++ JavaOCP/src/day12/Calc.java
@@ -1,9 +1,11 @@
 package day12;
 
 // Calc 計算器
-@FunctionalInterface
+@FunctionalInterface // 只能有一個未實現方法
 public interface Calc {
-	double compute(double x, double y); // compute 計算邏輯
+	
+	double compute(double x, double y); // compute 計算邏輯 (未實現方法)
+	String toString(); // 這是 Object.java 一定會實現的方法, 所以不列入未實現方法中
 	
 	default void copyright() {
 		System.out.println("版權所有");
