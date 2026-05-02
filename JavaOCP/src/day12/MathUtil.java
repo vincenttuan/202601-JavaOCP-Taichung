88138e615f63cbbdfebedd9cd7e2ef13764a1594
FP <FP@FP>
2026-05-02 10:35:03 +0800

    java

---
+++ JavaOCP/src/day12/MathUtil.java
@@ -0,0 +1,21 @@
+package day12;
+
+// 數學工具類
+public class MathUtil {
+	
+	public static double add(double x, double y) { // 加
+		return x + y;
+	}
+	
+	public static double sub(double x, double y) { // 減
+		return x - y;
+	}
+	
+	public static double mul(double x, double y) { // 乘
+		return x * y;
+	}
+	
+	public static double div(double x, double y) { // 除
+		return x / y;
+	}
+}
