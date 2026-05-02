c7a0f657e3b2da08a020288e8dec66b732d14ad8
FP <FP@FP>
2026-05-02 10:25:05 +0800

    java

--- JavaOCP/test/day12/TestCalc.java
+++ JavaOCP/test/day12/TestCalc.java
@@ -1,5 +1,17 @@
 package day12;
 
 public class TestCalc {
-
+	public static void main(String[] args) {
+		// 加法
+		// 1. 透過匿名內部類別來實現 Calc
+		Calc calc1 = new Calc() {
+			@Override
+			public double compute(double x, double y) {
+				return x + y;
+			}
+		};
+		System.out.println(calc1.compute(20.5, 10));
+		
+		
+	}
 }
