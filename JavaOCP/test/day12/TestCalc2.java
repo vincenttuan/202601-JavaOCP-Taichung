19f2dbfd3159159408014db84bc4cd04d78cc91b
FP <FP@FP>
2026-05-02 10:45:28 +0800

    lambda

--- JavaOCP/test/day12/TestCalc2.java
+++ JavaOCP/test/day12/TestCalc2.java
@@ -10,7 +10,13 @@ public class TestCalc2 {
 		// Lambda + :: (方法參考)
 		Calc calc2 = MathUtil::add;
 		System.out.println(calc2.compute(20.5, 10));
-
+		
+		Calc max1 = (x, y) -> x >= y ? x : y;
+		System.out.println(max1.compute(20.5, 10));
+		
+		Calc max2 = Math::max;
+		System.out.println(max2.compute(20.5, 10));
+		
 	}
 
 }
