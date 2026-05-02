3e1e09295479770f8f27dfff82d392c02a7e95e5
FP <FP@FP>
2026-05-02 11:35:52 +0800

    consumer

---
+++ JavaOCP/test/day12/TestComsumer.java
@@ -0,0 +1,16 @@
+package day12;
+
+/**
+@FunctionalInterface
+public interface Consumer<T> {
+    void accept(T t);
+}
+*/
+public class TestComsumer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
