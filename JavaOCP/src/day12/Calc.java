7c5c6763e153babe8c0726099bf83861b14032f5
FP <FP@FP>
2026-05-02 10:52:12 +0800

    lambda

--- JavaOCP/src/day12/Calc.java
+++ JavaOCP/src/day12/Calc.java
@@ -1,6 +1,15 @@
 package day12;
 
 // Calc 計算器
+@FunctionalInterface
 public interface Calc {
 	double compute(double x, double y); // compute 計算邏輯
+	
+	default void copyright() {
+		System.out.println("版權所有");
+	}
+	
+	static void welcome() {
+		System.out.println("歡迎使用");
+	}
 }
