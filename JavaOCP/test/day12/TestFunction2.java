69f6bdea0d023300df2aaad5d32a455ce442d622
FP <FP@FP>
2026-05-02 14:32:03 +0800

    function2

---
+++ JavaOCP/test/day12/TestFunction2.java
@@ -0,0 +1,37 @@
+package day12;
+
+/*
+ * 1個參數 + 1個回傳
+@FunctionalInterface
+public interface Function<T,R> {
+	R apply(T t);
+}
+
+ * 1個參數 + 1個回傳 且 參數與回傳型別相同 
+@FunctionalInterface
+public interface UnaryOperator<T> extends Function<T,T> {
+	// T apply(T t);
+}
+
+ * 2個參數 + 1個回傳
+@FunctionalInterface
+public interface BiFunction<T,U,R> {
+	R apply(T t, U u)
+}
+
+ * 2個參數 + 1個回傳 且 參數與回傳型別相同
+@FunctionalInterface
+public interface BinaryOperator<T> extends BiFunction<T,T,T> {
+	// T apply(T t1, T t2)
+}
+
+
+*/
+public class TestFunction2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
