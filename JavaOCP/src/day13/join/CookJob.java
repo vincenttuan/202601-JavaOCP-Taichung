1c6d42f29c8e3b288299cf1302ffd739d0c1cb71
FP <FP@FP>
2026-05-09 12:19:49 +0800

    thread

--- JavaOCP/src/day13/join/CookJob.java
+++ JavaOCP/src/day13/join/CookJob.java
@@ -5,13 +5,14 @@ public class CookJob implements Runnable {
 	@Override
 	public void run() {
 		String threadName = Thread.currentThread().getName();
+		System.out.printf("[%s]廚房開始做餐%n", threadName);
 		// 模擬花費的時間
 		try {
 			Thread.sleep(100); // 1000ms = 1s
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		System.out.printf("[%s]廚房做餐%n", threadName);
+		System.out.printf("[%s]廚房完成餐點%n", threadName);
 	}
 	
 }
