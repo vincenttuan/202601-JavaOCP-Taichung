c5ca434d35f987a79c9b720290cf88fdc4755342
FP <FP@FP>
2026-05-09 16:11:15 +0800

    java

--- JavaOCP/src/day13/travel/Main.java
+++ JavaOCP/src/day13/travel/Main.java
@@ -4,9 +4,13 @@ import java.util.concurrent.CyclicBarrier;
 
 public class Main {
 	public static void main(String[] args) {
+		// 到達之後要做的事 
+		Runnable barrierAction = () -> {
+			System.out.println("一起去吃午餐...");
+		};
 		// n 個人都到齊之後才出發
-		int parties = 3;
-		CyclicBarrier barrier = new CyclicBarrier(parties);
+		int parties = 4;
+		CyclicBarrier barrier = new CyclicBarrier(parties, barrierAction);
 		
 		Thread t1 = new Thread(new Traveler(barrier), "小明");
 		Thread t2 = new Thread(new Traveler(barrier), "小華");
