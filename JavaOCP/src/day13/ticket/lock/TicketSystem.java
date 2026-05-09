2ace5695e577356e5c4f2b3e4cf2d798e2eaf2a9
FP <FP@FP>
2026-05-09 14:44:56 +0800

    thread

--- JavaOCP/src/day13/ticket/lock/TicketSystem.java
+++ JavaOCP/src/day13/ticket/lock/TicketSystem.java
@@ -16,23 +16,26 @@ public class TicketSystem {
 		// 手動上鎖
 		lock.lock();
 		
-		if(tickets <= 0) {
-			System.out.printf("%s 沒看見票, 離開...%n", threadName);
-			return;
+		try {
+			if(tickets <= 0) {
+				System.out.printf("%s 沒看見票, 離開...%n", threadName);
+				return;
+			}
+			
+			System.out.printf("%s 看見有 %d 張票, 準備購買...%n", threadName, tickets);
+				
+			// 研擬買票程序會花費的時間延遲
+			Thread.sleep(100);
+				
+			// 買票
+			tickets--;
+			
+			System.out.printf("%s 成功買到 1 張票!%n", threadName);
+		} finally {
+			// 手動解鎖(保證會執行)
+			lock.unlock();
 		}
 		
-		System.out.printf("%s 看見有 %d 張票, 準備購買...%n", threadName, tickets);
-			
-		// 研擬買票程序會花費的時間延遲
-		Thread.sleep(100);
-			
-		// 買票
-		tickets--;
-		
-		System.out.printf("%s 成功買到 1 張票!%n", threadName);
-		
-		// 手動解鎖
-		lock.unlock();
 	}
 	
 }
