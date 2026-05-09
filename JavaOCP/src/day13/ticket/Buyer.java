25b40e61da240ba851b054ad577ccd33137f3d8e
FP <FP@FP>
2026-05-09 13:51:55 +0800

    ticket

---
+++ JavaOCP/src/day13/ticket/Buyer.java
@@ -0,0 +1,23 @@
+package day13.ticket;
+
+// 購票者
+public class Buyer implements Runnable {
+	
+	// 票務系統
+	private TicketSystem ticketSystem;
+	
+	public Buyer(TicketSystem ticketSystem) {
+		this.ticketSystem = ticketSystem;
+	}
+
+	@Override
+	public void run() {
+		try {
+			// 買票
+			ticketSystem.buy();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
