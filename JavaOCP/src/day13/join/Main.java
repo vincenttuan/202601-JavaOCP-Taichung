81a497d39ae40abb941f67f045237df853b7ca98
FP <FP@FP>
2026-05-09 12:17:05 +0800

    thread

--- JavaOCP/src/day13/join/Main.java
+++ JavaOCP/src/day13/join/Main.java
@@ -2,7 +2,7 @@ package day13.join;
 
 public class Main {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		Thread order1 = new Thread(new OrderJob(), "訂單 1");
 		Thread cook1 = new Thread(new CookJob(), "做餐 1");
 		
@@ -13,12 +13,15 @@ public class Main {
 		Thread cook3 = new Thread(new CookJob(), "做餐 3");
 		
 		order1.start();
+		order1.join(); // 等[訂單 1]完成
 		cook1.start();
 		
 		order2.start();
+		order2.join(); // 等[訂單 2]完成
 		cook2.start();
 		
 		order3.start();
+		order3.join(); // 等[訂單 3]完成
 		cook3.start();
 		
 	}
