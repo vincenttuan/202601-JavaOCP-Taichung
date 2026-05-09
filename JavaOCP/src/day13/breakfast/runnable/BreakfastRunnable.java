ecd7a30a940a932a780035259f60ddb944fc269b
FP <FP@FP>
2026-05-09 10:50:07 +0800

    thread

--- JavaOCP/src/day13/breakfast/runnable/BreakfastRunnable.java
+++ JavaOCP/src/day13/breakfast/runnable/BreakfastRunnable.java
@@ -1,21 +1,18 @@
 package day13.breakfast.runnable;
 
-import day13.breakfast.normal.CookJob;
-import day13.breakfast.normal.DrinkJob;
-import day13.breakfast.normal.OrderJob;
+import day13.breakfast.normal.Job;
 
 public class BreakfastRunnable implements Runnable {
 	
-	private Object job;
+	private Job job;
 	
-	public BreakfastRunnable(Object job) {
+	public BreakfastRunnable(Job job) {
 		this.job = job;
 	}
 	
 	@Override
 	public void run() {
-		
-		
+		job.work();
 	}
 
 }
