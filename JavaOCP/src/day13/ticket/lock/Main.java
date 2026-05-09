6cce8d95eb716ee5d99e3e650c0f9c5b9fd3afbb
FP <FP@FP>
2026-05-09 14:40:14 +0800

    thread

---
+++ JavaOCP/src/day13/ticket/lock/Main.java
@@ -0,0 +1,23 @@
+package day13.ticket.lock;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		TicketSystem ticketSystem = new TicketSystem();
+		
+		Thread t1 = new Thread(new Buyer(ticketSystem), "小明");
+		Thread t2 = new Thread(new Buyer(ticketSystem), "小華");
+		Thread t3 = new Thread(new Buyer(ticketSystem), "小英");
+		
+		t1.start();
+		/*
+		try {
+			Thread.sleep(50);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		*/
+		t2.start();
+		t3.start();
+	}
+}
---
+++ JavaOCP/src/day13/ticket/lock/TicketSystem.java
@@ -0,0 +1,26 @@
+package day13.ticket.lock;
+
+public class TicketSystem {
+	
+	private int tickets = 1; // 只有 1 張票
+	
+	public synchronized void buy() throws InterruptedException {
+		String threadName = Thread.currentThread().getName();
+		
+		if(tickets <= 0) {
+			System.out.printf("%s 沒看見票, 離開...%n", threadName);
+			return;
+		}
+		
+		System.out.printf("%s 看見有 %d 張票, 準備購買...%n", threadName, tickets);
+			
+		// 研擬買票程序會花費的時間延遲
+		Thread.sleep(100);
+			
+		// 買票
+		tickets--;
+		
+		System.out.printf("%s 成功買到 1 張票!%n", threadName);
+	}
+	
+}
