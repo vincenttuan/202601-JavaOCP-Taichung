4d44246fb75134e1d950ee8c04059c98cbe290df
FP <FP@FP>
2026-05-09 14:42:57 +0800

    thread

--- JavaOCP/src/day13/ticket/lock/TicketSystem.java
+++ JavaOCP/src/day13/ticket/lock/TicketSystem.java
@@ -1,11 +1,20 @@
 package day13.ticket.lock;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 public class TicketSystem {
 	
 	private int tickets = 1; // 只有 1 張票
 	
-	public synchronized void buy() throws InterruptedException {
+	// 建立高級智慧鎖 
+	private ReentrantLock lock = new ReentrantLock();
+	
+	// 不需要使用 synchronized
+	public void buy() throws InterruptedException {
 		String threadName = Thread.currentThread().getName();
+		
+		// 手動上鎖
+		lock.lock();
 		
 		if(tickets <= 0) {
 			System.out.printf("%s 沒看見票, 離開...%n", threadName);
@@ -21,6 +30,9 @@ public class TicketSystem {
 		tickets--;
 		
 		System.out.printf("%s 成功買到 1 張票!%n", threadName);
+		
+		// 手動解鎖
+		lock.unlock();
 	}
 	
 }
