b40e372f3202917fc00cbba437b1f5fa8674126d
FP <FP@FP>
2026-05-09 10:09:47 +0800

    thread

--- JavaOCP/src/day13/breakfast/normal/OrderJob.java
+++ JavaOCP/src/day13/breakfast/normal/OrderJob.java
@@ -1,11 +1,20 @@
 package day13.breakfast.normal;
 
+import java.util.concurrent.TimeUnit;
+
 public class OrderJob {
 	
 	public void work() {
 		
 		for(int i=1;i<=10;i++) {
 			System.out.printf("櫃台接單中: %d%n", i);
+			
+			// 模擬接單時間(每次接單時間會花費 1 秒鐘)
+			try {
+				TimeUnit.SECONDS.sleep(1);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 		}
 		
 	}
