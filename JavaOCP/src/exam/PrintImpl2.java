e4a9e90b468ffe52e40c6880a872b1c24c3725c2
FP <FP@FP>
2026-05-30 14:28:59 +0800

    java

---
+++ JavaOCP/src/exam/PrintImpl2.java
@@ -0,0 +1,13 @@
+package exam;
+
+public class PrintImpl2 implements Print {
+	
+	public PrintImpl2() {
+		
+	}
+	
+	@Override
+	public void print(String message) {
+		System.out.println("Print2: " + message);
+	}
+}
