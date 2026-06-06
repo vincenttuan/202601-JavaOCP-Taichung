0a62a0bf2d489b05a5f5cb0de4eef2f122d3f60e
FP <FP@FP>
2026-06-06 15:03:03 +0800

    web

--- JavaWeb/src/main/java/servlet/HelloServlet.java
+++ JavaWeb/src/main/java/servlet/HelloServlet.java
@@ -9,13 +9,13 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = {"/servlet/hello"})
+@WebServlet(urlPatterns = {"/servlet/hello", "/servlet/welcome"})
 public class HelloServlet extends HttpServlet {
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
-		String message = "Hello World " + new Date();
+		String message = "from " + req.getServletPath() + " Hello World " + new Date();
 		
 		// 包裝 HTTP Response 送出
 		resp.getWriter().print(message);
