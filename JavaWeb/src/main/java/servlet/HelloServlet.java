4f2e2fd94614b2ed0a9de475489e113706e160ba
FP <FP@FP>
2026-06-06 15:42:16 +0800

    web

--- JavaWeb/src/main/java/servlet/HelloServlet.java
+++ JavaWeb/src/main/java/servlet/HelloServlet.java
@@ -1,6 +1,7 @@
 package servlet;
 
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.Date;
 
 import jakarta.servlet.ServletException;
@@ -14,8 +15,31 @@ public class HelloServlet extends HttpServlet {
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// 編碼設定
+		req.setCharacterEncoding("UTF-8");
+		resp.setCharacterEncoding("UTF-8");
+		resp.setContentType("text/html;charset=UTF-8");
 		
-		String message = "from " + req.getServletPath() + " Hello World " + new Date();
+		String html ="""
+				<html>
+					<head>
+						<title>
+							Hello Page
+						</title>
+					</head>
+					<body>
+						<h1>
+							Hello World ! <p />
+						</h1>
+						<h2>
+							from: %s <p />
+							time: %s
+						</h2>
+					</body>
+				</html>
+				""";
+		
+		String message = html.formatted(req.getServletPath(), new Date());
 		
 		// 包裝 HTTP Response 送出
 		resp.getWriter().print(message);
