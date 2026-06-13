1af9392100d372a4a8d39d39cee49f69f73728b2
FP <FP@FP>
2026-06-13 12:21:36 +0800

    gb

--- JavaWeb/src/main/java/servlet/GuestbookServlet.java
+++ JavaWeb/src/main/java/servlet/GuestbookServlet.java
@@ -59,18 +59,45 @@ public class GuestbookServlet extends HttpServlet {
 								<legend>訪客留言版-歷史紀錄</legend>
 								資料筆數: %d <br />
 								資料內容: <br />
-								%s
+								<table border="1">
+									<thead>
+										<tr>
+											<th>序號</th><th>暱稱</th><th>留言內容</th><th>留言時間</th>
+										</tr>
+									</thead>
+									<tbody>
+										%s
+									</tbody>
+								<table>
 							</fieldset>
 						</div>
 					</body>
 				</html>
 				""";
 		
-		html = String.format(html, nickname, content, time, guestbooks.size(), guestbooks);
+		html = String.format(html, nickname, content, time, guestbooks.size(), getGuestbookTag(guestbooks));
 		
 		// 回應 html 內容
 		resp.getWriter().print(html);
+	}
+	
+	// 產生每一筆留言紀錄的 <tr> 標籤
+	private String getGuestbookTag(List<Guestbook> guestbooks) {
+		StringBuilder sb = new StringBuilder();
 		
+		for(int i=0;i<guestbooks.size();i++) {
+			Guestbook gb = guestbooks.get(i);
+			
+			String tr = """
+					<tr>
+						<td>%s</td><td>%s</td><td>%s</td><td>%s</td>
+					</tr>
+					""";
+			
+			sb.append(String.format(tr, gb.getId(), gb.getNickname(), gb.getContent(), gb.getTime()));
+		}
+		
+		return sb.toString();
 	}
 	
 }
