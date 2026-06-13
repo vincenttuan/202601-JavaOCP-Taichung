3b7a5a848a03acb3e7724feb1c919c52dcb1eba5
FP <FP@FP>
2026-06-13 10:40:22 +0800

    gb

--- JavaWeb/src/main/java/servlet/GuestbookServlet.java
+++ JavaWeb/src/main/java/servlet/GuestbookServlet.java
@@ -13,6 +13,11 @@ public class GuestbookServlet extends HttpServlet {
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// 進行編碼(讓中文不亂碼)
+		req.setCharacterEncoding("UTF-8"); // 請求(request)編碼 - 給 HTTP 看的
+		resp.setCharacterEncoding("UTF-8"); // 回應(response)編碼 - 給 HTTP 看的
+		resp.setContentType("text/html;charset=UTF-8"); // 給瀏覽器看的內容編碼
+		
 		// 取得 request 資料
 		String nickname = req.getParameter("nickname");
 		String content = req.getParameter("content");
