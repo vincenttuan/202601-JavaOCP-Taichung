830a6faa142195ec9ae8fc4af7b948e2aab0b66c
FP <FP@FP>
2026-06-13 11:57:17 +0800

    gb

--- JavaWeb/src/main/java/servlet/GuestbookServlet.java
+++ JavaWeb/src/main/java/servlet/GuestbookServlet.java
@@ -3,7 +3,10 @@ package servlet;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
+import dao.GuestbookDao;
+import entity.Guestbook;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -27,6 +30,13 @@ public class GuestbookServlet extends HttpServlet {
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss E");
 		String time = sdf.format(new Date()); // 留言時間
 		
+		// -- 儲存留言紀錄 ------------------------------
+		GuestbookDao dao = GuestbookDao.getInstance();
+		dao.add(nickname, content, time);
+		
+		// -- 查詢留言紀錄 ------------------------------
+		List<Guestbook> guestbooks = dao.queryAll();
+		
 		// html 內容
 		String html ="""
 				<html>
@@ -39,11 +49,20 @@ public class GuestbookServlet extends HttpServlet {
 						nickname = %s <br />
 						content = %s <br />
 						留言時間: %s <br />
+						<hr />
+						<div>
+							<fieldset>
+								<legend>訪客留言版-歷史紀錄</legend>
+								資料筆數: %d <br />
+								資料內容: <br />
+								%s
+							</fieldset>
+						</div>
 					</body>
 				</html>
 				""";
 		
-		html = String.format(html, nickname, content, time);
+		html = String.format(html, nickname, content, time, guestbooks.size(), guestbooks);
 		
 		// 回應 html 內容
 		resp.getWriter().print(html);
