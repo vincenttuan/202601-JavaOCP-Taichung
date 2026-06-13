d48b55ffec4f4ca5c4f5ca2348e852b1ef6758c3
FP <FP@FP>
2026-06-13 15:56:32 +0800

    query

---
+++ JavaWeb/src/main/java/servlet/GuestBookQueryServlet.java
@@ -0,0 +1,19 @@
+package servlet;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/servlet/query/guestbook")
+public class GuestBookQueryServlet extends HttpServlet {
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+	}
+	
+}
