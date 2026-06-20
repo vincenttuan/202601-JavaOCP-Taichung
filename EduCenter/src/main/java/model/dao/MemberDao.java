c110ee3ac2e4856673e55324f09d5b136856c50e
FP <FP@FP>
2026-06-20 12:22:40 +0800

    edu

--- EduCenter/src/main/java/model/dao/MemberDao.java
+++ EduCenter/src/main/java/model/dao/MemberDao.java
@@ -1,5 +1,49 @@
 package model.dao;
 
-public class MemberDao {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 
+import model.entity.Member;
+import model.util.DBUtil;
+
+public class MemberDao {
+	
+	// ===== Singleton 單例模式 =====
+	private static MemberDao instance = new MemberDao();
+	
+	private MemberDao() {
+		
+	}
+	
+	public static MemberDao getInsance() {
+		return instance;
+	}
+	
+	/**
+	 * 新增會員(註冊)
+	 * */
+	public void add(Member member) {
+		String sql = """
+				insert into member (username, password, fullname, email, role)
+				values(?, ?, ?, ?, ?)
+				""";
+		
+		try(Connection conn = DBUtil.getConnection();
+			PreparedStatement pstmt = conn.prepareStatement(sql)) {
+			
+			pstmt.setString(1, member.getUsername());
+			pstmt.setString(2, member.getPassword());
+			pstmt.setString(3, member.getFullname());
+			pstmt.setString(4, member.getEmail());
+			pstmt.setString(5, member.getRole());
+			
+			pstmt.executeUpdate();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
 }
