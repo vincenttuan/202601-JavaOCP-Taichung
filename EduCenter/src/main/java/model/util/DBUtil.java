9b36b812cc93e70e9b3581e2236e9be9b6362063
FP <FP@FP>
2026-06-20 12:14:46 +0800

    edu

--- EduCenter/src/main/java/model/util/DBUtil.java
+++ EduCenter/src/main/java/model/util/DBUtil.java
@@ -1,5 +1,26 @@
 package model.util;
 
-public class DBUtil {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+public class DBUtil {
+	
+	private static final String DB_URL = "jdbc:mysql://localhost:3306/educenter?useSSL=false&serverTimeZone=Asia/Taipei";
+	private static final String USERNAME = "root";
+	private static final String PASSWORD = "12345678";
+	
+	static {
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver"); // 動態載入 driver
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public static Connection getConnection() throws Exception {
+		return DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
+	}
+	
 }
