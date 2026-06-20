cba90f16c21379db8704e187832cf5f82b645c4f
FP <FP@FP>
2026-06-20 14:37:23 +0800

    edu

--- EduCenter/src/main/java/controller/member/RegisterServlet.java
+++ EduCenter/src/main/java/controller/member/RegisterServlet.java
@@ -8,6 +8,8 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import model.dao.MemberDao;
+import model.entity.Member;
 
 @WebServlet(urlPatterns = {"/register"})
 public class RegisterServlet extends HttpServlet {
@@ -18,6 +20,39 @@ public class RegisterServlet extends HttpServlet {
 		// 建立請求分派器
 		RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/view/register.jsp");
 		rd.forward(req, resp);
+		
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// 設定編碼
+		req.setCharacterEncoding("UTF-8");
+		resp.setCharacterEncoding("UTF-8");
+		resp.setContentType("text/html;utf-8");
+		
+		// 取得表單資料
+		String username = req.getParameter("username");
+		String password = req.getParameter("password");
+		String fullname = req.getParameter("fullname");
+		String email = req.getParameter("email");
+		String role = req.getParameter("role");
+		
+		// 建立 Member 物件
+		Member member = new Member();
+		member.setUsername(username);
+		member.setPassword(password);
+		member.setFullname(fullname);
+		member.setEmail(email);
+		member.setRole(role);
+		
+		// 取得 Dao 物件
+		MemberDao memberDao = MemberDao.getInstance();
+		
+		// 註冊會員資料(將資料存放到資料庫中)
+		memberDao.register(member);
+		
+		// 回應
+		resp.getWriter().print("Register OK");
 		
 	}
 	
