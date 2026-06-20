61c92b6b765eaa8f6b70346ba9c21dad2cde546b
FP <FP@FP>
2026-06-20 12:13:15 +0800

    edu

--- EduCenter/src/main/java/model/entity/Member.java
+++ EduCenter/src/main/java/model/entity/Member.java
@@ -1,5 +1,18 @@
 package model.entity;
 
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
 public class Member {
+	
+	private Integer id;
+	private String username;
+	private String password;
+	private String fullname;
+	private String email;
+	private String role;
+	private Date createTime;
 	
 }
