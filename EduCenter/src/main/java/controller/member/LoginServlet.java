7149921b7dbf123c8ab440d2c6d1e2aec8d507dd
FP <FP@FP>
2026-06-27 10:35:16 +0800

    login

--- EduCenter/src/main/java/controller/member/LoginServlet.java
+++ EduCenter/src/main/java/controller/member/LoginServlet.java
@@ -8,6 +8,8 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import model.dao.MemberDao;
+import model.entity.Member;
 
 @WebServlet(urlPatterns = {"/login"})
 public class LoginServlet extends HttpServlet {
@@ -18,6 +20,30 @@ public class LoginServlet extends HttpServlet {
 		// 建立請求分派器
 		RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/view/login.jsp");
 		rd.forward(req, resp);
+		
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// 接收網頁表單內容 利用 getParameter()
+		String username = req.getParameter("username");
+		String password = req.getParameter("password");
+		
+		// 驗證帳密
+		Member member = MemberDao.getInstance().login(username, password);
+		
+		// 若 member = null 表示沒有找到該會員資料也就是登入失敗
+		if(member == null) {
+			// 建立請求分派器
+			RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/view/login.jsp");
+			req.setAttribute("result", "Login fail (登入失敗)");
+			rd.forward(req, resp);
+		} else {
+			// 建立請求分派器
+			RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/view/login.jsp");
+			req.setAttribute("result", "Login OK (登入成功)");
+			rd.forward(req, resp);
+		}
 		
 	}
 	
