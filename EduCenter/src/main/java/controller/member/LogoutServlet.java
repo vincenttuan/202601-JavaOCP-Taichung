1895248c94cb219bd76be47177c420e26bc4761e
FP <FP@FP>
2026-06-27 14:01:13 +0800

    logout

--- EduCenter/src/main/java/controller/member/LogoutServlet.java
+++ EduCenter/src/main/java/controller/member/LogoutServlet.java
@@ -1,5 +1,26 @@
 package controller.member;
 
-public class LogoutServlet {
+import java.io.IOException;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+@WebServlet(urlPatterns = {"/logout"})
+public class LogoutServlet extends HttpServlet {
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		HttpSession session = req.getSession();
+		session.invalidate(); // session 失效
+		
+		// 自動重新指向登入頁面
+		resp.sendRedirect("/EduCetner/login");
+		
+	}
+	
 }
