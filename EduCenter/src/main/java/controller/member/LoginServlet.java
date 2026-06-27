8092427be0318c5b8753c1b61d0d803fa8505b93
FP <FP@FP>
2026-06-27 10:05:19 +0800

    login

--- EduCenter/src/main/java/controller/member/LoginServlet.java
+++ EduCenter/src/main/java/controller/member/LoginServlet.java
@@ -1,5 +1,24 @@
 package controller.member;
 
-public class LoginServlet {
+import java.io.IOException;
 
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = {"/login"})
+public class LoginServlet extends HttpServlet {
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		// 建立請求分派器
+		RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/view/login.jsp");
+		rd.forward(req, resp);
+		
+	}
+	
 }
