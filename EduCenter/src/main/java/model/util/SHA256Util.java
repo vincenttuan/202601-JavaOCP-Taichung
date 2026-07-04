9bf518590dd04f844ad9734a093fcaf0d6166fb0
FP <FP@FP>
2026-07-04 14:44:20 +0800

    web

--- EduCenter/src/main/java/model/util/SHA256Util.java
+++ EduCenter/src/main/java/model/util/SHA256Util.java
@@ -2,11 +2,12 @@ package model.util;
 
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
+import java.security.SecureRandom;
 
 public class SHA256Util {
 	
 	/**
-	 * SHA-256 加密
+	 * SHA-256 加密 + Salt hash
 	 * 功能:
 	 * 將使用者所輸入的文字經過 SHA-256 演算法後
 	 * 轉成 64 個 16 進位的字元(hash)
@@ -17,15 +18,19 @@ public class SHA256Util {
 	 * 
 	 * */
 	
-	public static String hash(String text) {
+	public static String hash(String text, String salt) {
 		try {
+			
+			// 密碼加鹽
+			String textSalt = text + salt;
+			
 			// 1. 取得 SHA-256 的 MessageDigest 物件
 			// MessageDigest 是 Java 提供的雜湊(hash)工具
 			MessageDigest md = MessageDigest.getInstance("SHA-256");
 			
 			// 2. 將 String 轉 byte[]
 			// 因為 SHA-256 只能處理位元組資料(byte[])
-			byte[] textBytes = text.getBytes(StandardCharsets.UTF_8);
+			byte[] textBytes = textSalt.getBytes(StandardCharsets.UTF_8);
 			
 			// 3. 執行 SHA-256
 			byte[] hashBytes = md.digest(textBytes);
@@ -45,6 +50,21 @@ public class SHA256Util {
 		}
 		return null;
 	} 
+	
+	// 產生鹽
+	public static String generateSalt() {
+		
+		SecureRandom random = new SecureRandom();
+		byte[] bytes = new byte[16];
+		random.nextBytes(bytes);
+		
+		StringBuilder sb = new StringBuilder();
+		for(byte b : bytes) {
+			sb.append(String.format("%02x", b));
+		}
+		
+		return sb.toString();
+	}
 	
 	
 }
