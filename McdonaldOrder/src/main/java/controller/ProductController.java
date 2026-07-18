2d7780ae9425c66a997c52752a6a76fb7ad86b58
FP <FP@FP>
2026-07-18 12:06:44 +0800

    web

--- McdonaldOrder/src/main/java/controller/ProductController.java
+++ McdonaldOrder/src/main/java/controller/ProductController.java
@@ -1,5 +1,14 @@
 package controller;
 
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 /**
  * 商品 Controller
  * 
@@ -17,6 +26,39 @@ package controller;
  * 
  * */
 
-public class ProductController {
-
+@WebServlet("/products")
+public class ProductController extends HttpServlet {
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String action = req.getParameter("action");
+		String id = req.getParameter("id");
+		
+		switch(action) {
+			case "new" -> showCreateForm(req, resp);
+			case "edit" -> showEditForm(req, resp);
+			default -> showList(req, resp);
+		}
+		
+	}
+	
+	// 顯示新增表單
+	private void showCreateForm(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/views/product_form.jsp");
+		rd.forward(req, resp);
+		
+	}
+	
+	// 顯示修改表單
+	private void showEditForm(HttpServletRequest req, HttpServletResponse resp) {
+		
+	}
+	
+	// 顯示商品列表
+	private void showList(HttpServletRequest req, HttpServletResponse resp) {
+		
+	}
+	
+	
 }
