fb62d26c851287aeb067d039d12548eca1cd8c3c
FP <FP@FP>
2026-07-18 14:23:05 +0800

    web

--- McdonaldOrder/src/main/java/filter/EncodingFilter.java
+++ McdonaldOrder/src/main/java/filter/EncodingFilter.java
@@ -1,5 +1,32 @@
 package filter;
 
-public class EncodingFilter {
+import java.io.IOException;
 
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * 統一處理 UTF-8, 避免中文亂碼 
+ * */
+@WebFilter(urlPatterns = {"/products"})
+public class EncodingFilter extends HttpFilter {
+	
+	@Override
+	protected void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		
+		// 編碼
+		request.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html;charset=UTF-8");
+		
+		// 往下繼續執行
+		chain.doFilter(request, response);
+		
+	}
+	
 }
