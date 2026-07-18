9abfba46fc0ae16d32ef8ef75cf03ad9dedafa16
FP <FP@FP>
2026-07-18 14:50:09 +0800

    web

--- McdonaldOrder/src/main/java/model/entity/Product.java
+++ McdonaldOrder/src/main/java/model/entity/Product.java
@@ -1,5 +1,22 @@
 package model.entity;
 
-public class Product {
+import java.time.LocalDateTime;
 
+import lombok.Data;
+
+/**
+ * Product Entity
+ * 對應 MySQL product 資料表
+ * */
+@Data
+public class Product {
+	private Long id;
+	private String name;
+	private String category;
+	private Integer price;
+	private Integer stock;
+	private String imageBase64;
+	private String imageType;
+	private LocalDateTime createAt;
+	private LocalDateTime updateAt;
 }
