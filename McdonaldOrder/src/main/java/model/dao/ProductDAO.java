a80155eef345ca7ab3e53de55aff21e2dea63773
FP <FP@FP>
2026-07-18 14:57:26 +0800

    web

--- McdonaldOrder/src/main/java/model/dao/ProductDAO.java
+++ McdonaldOrder/src/main/java/model/dao/ProductDAO.java
@@ -1,5 +1,40 @@
 package model.dao;
 
-public class ProductDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 
+import model.entity.Product;
+import util.DBUtil;
+
+/**
+ * ProductDAO
+ * 負責 JDBC 與 SQL
+ * 資料使用 Product (Entity)
+ * */
+public class ProductDAO {
+	
+	public void insert(Product product) {
+		String sql = """
+				insert into product(name, category, price, stock, image_base64, image_type)
+				values(?, ?, ?, ?, ?, ?)
+				""";
+		
+		try(Connection conn = DBUtil.getConnection();
+			PreparedStatement pstmt = conn.prepareStatement(sql)) {
+			
+			pstmt.setLong(1, product.getId());
+			pstmt.setString(2, product.getName());
+			pstmt.setInt(3, product.getPrice());
+			pstmt.setInt(4, product.getStock());
+			pstmt.setString(5, product.getImageBase64());
+			pstmt.setString(5, product.getImageType());
+			
+			pstmt.executeUpdate();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
 }
