a65135f94a1b3f52d79bec73c8f02e178d739361
FP <FP@FP>
2026-07-18 12:00:10 +0800

    web

--- McdonaldOrder/src/main/java/controller/ProductController.java
+++ McdonaldOrder/src/main/java/controller/ProductController.java
@@ -1,5 +1,22 @@
 package controller;
 
+/**
+ * 商品 Controller
+ * 
+ * GET:
+ * 給予 UI 畫面
+ * 商品列表 /products?action=list 或 /products
+ * 商品新增 /products?action=new
+ * 商品修改 /products?action=edit&id=1
+ * 
+ * POST:
+ * 修改資料庫
+ * 新增 /products?action=insert
+ * 修改 /products?action=update
+ * 刪除 /products?action=delete
+ * 
+ * */
+
 public class ProductController {
 
 }
