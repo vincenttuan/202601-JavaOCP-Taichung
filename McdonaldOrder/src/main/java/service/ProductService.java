a970fddbe07e7275f96e29ce43b73baae5885372
FP <FP@FP>
2026-07-18 15:06:33 +0800

    web

--- McdonaldOrder/src/main/java/service/ProductService.java
+++ McdonaldOrder/src/main/java/service/ProductService.java
@@ -1,5 +1,35 @@
 package service;
 
-public class ProductService {
+import model.dao.ProductDAO;
+import model.dto.ProductDTO;
+import model.entity.Product;
 
+/**
+ * Product Service
+ * 負責資料驗證,商業流程與 DTO / Entity 之間的轉換
+ * */
+public class ProductService {
+	
+	private ProductDAO productDAO = new ProductDAO();
+	
+	public void create(ProductDTO dto) {
+		// productDTO(DTO) 轉 product(Entity)
+		Product product = toEntity(dto);
+		// 儲存
+		productDAO.insert(product);
+	}
+	
+	// 轉 entity
+	private Product toEntity(ProductDTO dto) {
+		Product product = new Product();
+		product.setId(dto.getId());
+		product.setName(dto.getName());
+		product.setCategory(dto.getCategory());
+		product.setPrice(dto.getPrice());
+		product.setStock(dto.getStock());
+		product.setImageBase64(dto.getImageBase64());
+		product.setImageType(dto.getImageType());
+		return product;
+	}
+	
 }
