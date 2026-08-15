abad5456e29a68ac9ee7d48f1faf2b81c6420b9f
FP <FP@FP>
2026-08-15 11:45:18 +0800

    web

--- McdonaldOrder/src/main/java/model/dto/OrderItemDTO.java
+++ McdonaldOrder/src/main/java/model/dto/OrderItemDTO.java
@@ -1,5 +1,18 @@
 package model.dto;
 
-public class OrderItemDTO {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderItemDTO {
+	
+	private Long productId;
+	private String productName;
+	private Integer unitPrice;
+	private Integer quantity;
+	
+	
 }
