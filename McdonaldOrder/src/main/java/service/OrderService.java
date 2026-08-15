bd46728b591679605a78c48584c1d89549b9e6ee
FP <FP@FP>
2026-08-15 13:53:23 +0800

    web

---
+++ McdonaldOrder/src/main/java/service/OrderService.java
@@ -0,0 +1,18 @@
+package service;
+
+import java.util.List;
+import java.util.Map;
+
+import model.dto.OrderItemDTO;
+import model.dto.ProductDTO;
+
+public class OrderService {
+	
+	// 結帳服務
+	// 將 Map<ProductDTO, Integer> cart 轉 List<OrderItemDTO> items
+	public void checkout(String customerName, String customerPhone, Map<ProductDTO, Integer> cart) {
+		
+		
+	}
+	
+}
