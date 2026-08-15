2baaa44a08afb01eb329ce3a2a70354a8ae98ba9
FP <FP@FP>
2026-08-15 11:43:32 +0800

    web

--- McdonaldOrder/src/main/java/model/dto/OrderDTO.java
+++ McdonaldOrder/src/main/java/model/dto/OrderDTO.java
@@ -1,8 +1,12 @@
 package model.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class OrderDTO {
 	
 	private Long orderId;
