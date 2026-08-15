148efa16e4abed44379cf26065fd38cd586465f5
FP <FP@FP>
2026-08-15 12:01:46 +0800

    web

--- McdonaldOrder/src/main/java/model/dao/OrderDAO.java
+++ McdonaldOrder/src/main/java/model/dao/OrderDAO.java
@@ -1,14 +1,67 @@
 package model.dao;
 
+import java.sql.Connection;
 import java.util.List;
 
 import model.dto.OrderDTO;
 import model.dto.OrderItemDTO;
+import util.DBUtil;
 
 public class OrderDAO {
 	
-	// 建立訂單
-	public OrderDTO createOrder(String customerName, String customerPhone, List<OrderItemDTO> itemds) {
+	/** 建立訂單 
+	 * 
+	 * Transaction 交易
+	 * --------------------
+	 * 1. 查詢商品
+	 * 2. 查詢庫存
+	 * 3. 計算總金額
+	 * 4. 建立 CustomerOrder
+	 * 5. 建立 OrderItem
+	 * 6. 扣除庫存
+	 * --------------------
+	 * 
+	 * 成功 -> commit (提交任務)
+	 * 
+	 * 失敗 -> Rollback (回滾)
+	 * 
+	 * */
+	public OrderDTO createOrder(String customerName, String customerPhone, List<OrderItemDTO> items) {
+		
+		// 查詢商品 sql 與 庫存
+		String productSql = """
+				select id, name, price, stock
+				from product
+				where id = ?
+				""";
+		
+		// 新增訂單 sql
+		String orderSql = """
+				insert into customer_order (customer_name, customer_phone, total_amount, status)
+				values(?, ?, ?, 'PAID');
+				""";
+		
+		// 新增訂單項目 sql
+		String itemSql = """
+				insert into order_item (order_id, product_id, product_name, unit_price, quantity, subtotal)
+				values(?, ?, ?, ?, ?, ?);
+				""";
+		
+		// 修改商品庫存 sql
+		String stockSql = """
+				update product
+				set stock = stock - ?
+				where id = ?
+				""";
+		
+		// 資料庫處理程序
+		try(Connection conn = DBUtil.getConnection()) {
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new RuntimeException("結帳失敗", e);
+		}
+		
 		
 		return null;
 	}
