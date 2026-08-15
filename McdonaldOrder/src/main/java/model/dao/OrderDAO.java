2b69d2d11337a4645e8118bae6f5486c3ed4a6ea
FP <FP@FP>
2026-08-15 11:48:35 +0800

    web

---
+++ McdonaldOrder/src/main/java/model/dao/OrderDAO.java
@@ -0,0 +1,17 @@
+package model.dao;
+
+import java.util.List;
+
+import model.dto.OrderDTO;
+import model.dto.OrderItemDTO;
+
+public class OrderDAO {
+	
+	// 建立訂單
+	public OrderDTO createOrder(String customerName, String customerPhone, List<OrderItemDTO> itemds) {
+		
+		return null;
+	}
+	
+	
+}
