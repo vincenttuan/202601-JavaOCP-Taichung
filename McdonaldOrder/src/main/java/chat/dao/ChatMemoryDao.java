688530a734f45f60e1550a7041575961c3066305
FP <FP@FP>
2026-08-29 10:21:45 +0800

    web

--- McdonaldOrder/src/main/java/chat/dao/ChatMemoryDao.java
+++ McdonaldOrder/src/main/java/chat/dao/ChatMemoryDao.java
@@ -1,5 +1,13 @@
 package chat.dao;
 
-public interface ChatMemoryDao {
+import java.util.List;
 
+import chat.model.ChatMessage;
+
+public interface ChatMemoryDao {
+	
+	void save(ChatMessage message); // 儲存一筆訊息
+	
+	List<ChatMessage> findAll(); // 取得目前訊息
+	
 }
